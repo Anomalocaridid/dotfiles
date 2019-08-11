35dc265884e98fee2c55fe9a5dafade8d06bc8d0
DERPSQUID9 <DERPSQUID@protonmail.com>
2019-08-11 12:18:18 -0400

    Removed Xmonad config files because it causes issues with my setup

--- .xmonad/xmonad.hs
+++
@@ -1,7 +0,0 @@
-import           XMonad
-import           XMonad.Config.Xfce
-
-main = xmonad xfceConfig
-        { normalBorderColor = "#000B1E"
-        , focusedBorderColor = "#711C91"
-        }
