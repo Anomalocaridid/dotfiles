a64e2b3dda509029e43dcb01248665ca9057af0d
Anomalocaridid <DERPSQUID@protonmail.com>
2021-05-13 11:37:07 -0400

    Wezterm: changed colors of selected text

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -16,8 +16,11 @@ return {
         foreground = "#0abdc6",
         background = "#000b1e",
 
-        cursor_bg = "#0abdc6",
         cursor_fg = "#000b1e",
+        cursor_bg = "#0abdc6",
+
+        selection_fg = "#133e7c",
+        selection_bg = "#711c91",
 
         ansi = {"#123e7c", "#ff0000", "#d300c4", "#f57800", "#123e7c", "#711c91", "#0abdc6", "#d7d7d5"},
         brights = {"#1c61c2", "#ff0000", "#d300c4", "#f57800", "#00ff00", "#711c91", "#0abdc6", "#d7d7d5"},
