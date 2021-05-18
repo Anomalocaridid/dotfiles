52de60340e0ba7f0058c916fefd4e7d9783ba909
Anomalocaridid <DERPSQUID@protonmail.com>
2021-05-18 18:45:47 -0400

    Wezterm: Added split and scrollbar thumb colors to color scheme.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,18 +1,17 @@
 local wezterm = require 'wezterm';
 
 return {
-	color_scheme = "Cyberpunk Neon",
     font_size = 11.0,
     font = wezterm.font("FiraCode Nerd Font"),
     custom_block_glyphs = false,
 	hide_tab_bar_if_only_one_tab = true,
 	window_background_opacity = 0.90,
 	bold_brightens_ansi_colors = false,
-
 	exit_behavior = "Close",
-
 	check_for_updates = false,
-    
+
+	color_scheme = "Cyberpunk Neon",
+
     colors = {
         tab_bar = {
             background = "#000b1e",
