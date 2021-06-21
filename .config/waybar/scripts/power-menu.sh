3c164ab4c471f6c4835f9764ade0f4518e75f072
Anomalocaridid <DERPSQUID@protonmail.com>
2021-06-20 21:18:21 -0400

    feat: add hibernate option in power menu

--- .config/waybar/scripts/power-menu.sh
+++ .config/waybar/scripts/power-menu.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-entries="Logout Suspend Reboot Shutdown"
+entries=(Logout Suspend Hibernate Reboot Shutdown)
 
-selected=$(printf '%s\n' $entries | wofi --conf=$HOME/.config/wofi/config.power --style=$HOME/.config/wofi/style.widgets.css | awk '{print tolower($1)}')
+selected=$(printf '%s\n' "${entries[@]}" | wofi --conf="$HOME/.config/wofi/config.power" --style="$HOME/.config/wofi/style.widgets.css" | awk '{print tolower($1)}')
 
 case $selected in
-  logout)
-    swaymsg exit;;
-  suspend)
-    exec systemctl suspend;;
-  reboot)
-    exec systemctl reboot;;
-  shutdown)
-    exec systemctl poweroff -i;;
+	logout)
+		swaymsg exit
+		;;
+	suspend)
+		exec systemctl suspend
+		;;
+	hibernate)
+		exec systemctl hibernate
+		;;
+	reboot)
+		exec systemctl reboot
+		;;
+	shutdown)
+		exec systemctl poweroff -i
+		;;
 esac
