5f0c5aed7f8dae2ba272535685a40b169177514e
Anomalocaridid <DERPSQUID@protonmail.com>
2022-01-07 20:00:47 -0500

    refactor(zsh): lazy load zsh plugins with zcomet trigger where possible

--- .config/zsh/config.d/a_first.zsh
+++ .config/zsh/config.d/a_first.zsh
@@ -13,16 +13,16 @@ source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
 zcomet load romkatv/powerlevel10k
 
 # Plugins
-zcomet load	Aloxaf/fzf-tab # Use fzf for tab completion
-zcomet load	amstrad/oh-my-matrix # Falling text
-zcomet load	hlissner/zsh-autopair # Automatically close and delete paired delimiters
-zcomet load	Tarrasch/zsh-bd # Move to higher level directory
+zcomet load Aloxaf/fzf-tab # Use fzf for tab completion
+zcomet load hlissner/zsh-autopair # Automatically close and delete paired delimiters
 zcomet load zsh-users/zsh-history-substring-search # Fish-style history search
+zcomet trigger bd Tarrasch/zsh-bd # Move to higher level directory
+zcomet trigger matrix amstrad/oh-my-matrix # Falling text
 
 # Oh My Zsh plugins
-zcomet load ohmyzsh plugins/command-not-found # Shows which package an uninstalled command is in
-zcomet load ohmyzsh plugins/fancy-ctrl-z # Hit Ctrl z instead of running fg
 zcomet load ohmyzsh lib/termsupport.zsh # Set terminal window title
+zcomet load ohmyzsh plugins/fancy-ctrl-z # Hit Ctrl z instead of running fg
+zcomet trigger command_not_found_handler ohmyzsh plugins/command-not-found # Shows which package an uninstalled command is in
 
 # Snippets
 zcomet snippet https://github.com/jarun/nnn/blob/master/misc/quitcd/quitcd.bash_zsh
