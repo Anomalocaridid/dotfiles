e93c71bbff9808304d838a1473754e10c49c952e
Anomalocaridid <DERPSQUID@protonmail.com>
2022-01-12 21:01:23 -0500

    feat(nnn): configure bulknew, chksum plugins, add ~/.config bookmark

--- .config/zsh/config.d/nnn.zsh
+++ .config/zsh/config.d/nnn.zsh
@@ -1,10 +1,11 @@
+# ~/.config/zsh/config.d/nnn.zsh
 # nnn config
 
 export NNN_OPTS="cDEirx"
 export NNN_OPENER="$HOME/.config/nnn/plugins/nuke"
 export GUI=1
-export NNN_BMS="d:$HOME/Documents;D:$HOME/Downloads;p:$HOME/Pictures;v:$HOME/Videos;e:$HOME/exercism"
-export NNN_PLUG="s:!zsh -i;z:autojump;u:getplugs;d:dragdrop;k:kdeconnect;m:nmount"
+export NNN_BMS="c:$HOME/.config;d:$HOME/Documents;D:$HOME/Downloads;e:$HOME/exercism;p:$HOME/Pictures;v:$HOME/Videos;"
+export NNN_PLUG="c:chksum;d:dragdrop;k:kdeconnect;M:nmount;n:bulknew;s:!zsh -i;u:getplugs;z:autojump;"
 export NNN_FIFO="/tmp/nnn.fifo"
 export NNN_COLORS=#1909c9d02e0d2cff
 
