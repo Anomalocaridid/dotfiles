2c1fe0cd4a2abf6761580b98b3d5e5ee59c76513
Anomalocaridid <DERPSQUID@protonmail.com>
2022-02-07 17:02:29 -0500

    fix(waybar): fix pacman segment script not always indicating if reboots are necessary

--- .config/waybar/scripts/pacman.py
+++ .config/waybar/scripts/pacman.py
@@ -16,28 +16,40 @@ REGEX = r"(?<=Target\s=\s).*"
 # Icon for segment
 ICON = ""
 
-with open(FILE) as package_list:
-    text = package_list.read()
+# Get list of available updates
+update_list = (
+    sub.run("checkupdates", stdout=sub.PIPE).stdout.decode("utf-8").split()[::4]
+)
 
-reboot_packages = re.findall(REGEX, text)
+# Get number of updates
+update_count = len(update_list)
 
-reboot_regex = "|".join(map(fn.translate, reboot_packages))
-
-update_list = sub.run("checkupdates", stdout=sub.PIPE).stdout.decode("utf-8")
-
-update_count = len(update_list.split("\n")) - 1
-
+# Add update count to output object
 output = {
     "text": f"{ICON} {update_count}",
 }
 
+# Indicate whether system is up to date or how many updates are available
 if update_count == 0:
     output["tooltip"] = "Up to date"
 else:
+    # Otherwise, have it say how many updates are available
     output["tooltip"] = f"{update_count} updates available"
 
-if re.search(reboot_regex, update_list) is not None:
+# Read file with packages
+with open(FILE) as package_list:
+    text = package_list.read()
+
+# Extract list of package glob patterns from FILE
+reboot_packages = re.findall(REGEX, text)
+
+# Translate globs to regex, combine into one regex, and remove occurances of \Z
+reboot_regex = "|".join(map(fn.translate, reboot_packages)).replace(r"\Z", "")
+
+# If any of the available updates would need a reboot, indicate it
+if re.search(reboot_regex, " ".join(update_list)) is not None:
     output["text"] += "!"
     output["tooltip"] += ", reboot after update recommended"
 
+# Print output as json with unicode
 print(json.dumps(output, ensure_ascii=False))
