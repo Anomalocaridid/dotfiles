e000aada9084fa6b5ffcbd39b91499ee861625e7
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2022-04-27 15:22:35 -0400

    feat(swaylock): change lock screen to snapshot of wallpaper

--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -1,4 +1,4 @@
-#!/bin/env sh
+#!/bin/env bash
 # Times the screen off and puts it to background
 swayidle \
     timeout  300 'swaymsg "output * dpms off"' \
@@ -7,9 +7,18 @@ swayidle \
 workspace=$(swaymsg -t get_workspaces | jq "map(select(.focused)) | .[].num")
 # Goes to empty workspace
 swaymsg workspace number 0
+# Hides bar
+swaymsg bar mode invisible
 # Locks the screen immediately
-swaylock
+# Detaches swaylock to run next commands while locked
+swaylock --daemonize
+# Shows bar
+swaymsg bar mode dock
 # Restores previous workspace
 swaymsg workspace number "$workspace"
+# Waits for swaylock to terminate so idle timer is not prematurely killed
+while pgrep swaylock &> /dev/null; do
+    :
+done
 # Kills last background task so idle timer doesn't keep running
 kill %%
