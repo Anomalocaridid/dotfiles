1087bf407da1452ad1333418f0a1466275d0484f
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2022-07-22 12:32:51 -0400

    feat(nyxt): return mode visibility to defaults, remove extra glyphs

--- .config/nyxt/config.d/status.lisp
+++ .config/nyxt/config.d/status.lisp
@@ -5,17 +5,6 @@
 (define-configuration status-buffer
   ((glyph-mode-presentation-p t)))
 
-;;; make all modes visible
-(define-configuration mode
-  ((visible-in-status-p t)))
-
-;(defmacro define-glyphs (&rest glyphs)
-;  "Helper macro to set `glyph' slot for multiple modes at once."
-;  `(progn ,@(loop for (mode glyph)
-;                  in glyphs 
-;                  collect `(define-configuration ,mode
-;                             ((glyph ,glyph))))))
-
 (defmacro define-glyphs (&rest glyphs)
   "Helper macro to set `glyph' slot for multiple modes at once."
   `(progn ,@(loop for (mode glyph raw-p)
@@ -26,27 +15,5 @@
                              ((glyph ,glyph))))))
 
 ;;; define glyphs for modes
-(define-glyphs (autofill-mode               "")
-               (annotate-mode               "")
-               (base-mode                   "" t)
-               ; use when nerd font code point conflicts have been resolved
-               ;(base-mode                  "爵" t)
-               (blocker-mode                "")
-               (bookmark-mode               "")
-               (certificate-exception-mode  "")
-               (document-mode               "")
-               (help-mode                   "")
-               (hint-mode                   "")
-               (history-mode                "")
-               (message-mode                "")
-               (password-mode               "ﳳ")
-               (prompt-buffer-mode          "")
-               (reduce-tracking-mode        "")
-               (repl-mode                   "")
-               (search-buffer-mode          "")
-               (small-web-mode              "ﰍ")
-               (spell-check-mode            "")
-               ; use when nerd font code point conflicts have been resolved
-               ;(spell-check-mode           "暈")
-               (nyxt/vi-mode:vi-insert-mode "" t)
-               (nyxt/vi-mode:vi-normal-mode "" t))
+(define-glyphs (blocker-mode                "")
+               (reduce-tracking-mode        ""))
