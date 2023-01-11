f2a13d2a82728aa223a0bd533407a6123b368212
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-01-11 10:42:10 -0500

    feat(wezterm): configure fira code ligature variants

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,6 @@
 local wezterm = require 'wezterm';
 
 return {
-	font_size = 11.0,
-	font = wezterm.font("FiraCode Nerd Font"),
 	hide_tab_bar_if_only_one_tab = true,
 	window_background_opacity = 0.90,
 	bold_brightens_ansi_colors = false,
@@ -11,8 +9,22 @@ return {
 	enable_wayland = true,
 	enable_kitty_graphics = true,
 
-	color_scheme = "Cyberpunk Neon",
+	font_size = 11.0,
+	font = wezterm.font {
+		family = "FiraCode Nerd Font",
+		harfbuzz_features = {
+			"ss09", -- >>= <<= ||= |=
+			"cv25", -- .-
+			"cv26", -- :-
+			"cv32", -- .=
+			"cv27", -- []
+			"cv28", -- {. .}
+			"ss07", -- =~ !~
+			"ss10", -- Fl Tl fi fj fl ft
+		},
+	},
 
+	color_scheme = "Cyberpunk Neon",
 	colors = {
 		tab_bar = {
 			background = "#000b1e",
