d1e48b54f82f53b9ba29be2d69f8966df4e76311
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-04-08 16:36:55 -0400

    refactor(nix): clean up boot settings

--- configuration.nix
+++ configuration.nix
@@ -29,8 +29,10 @@
   };
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
   networking.hostName = "home-pc"; # Define your hostname.
   # Pick only one of the below networking options.
