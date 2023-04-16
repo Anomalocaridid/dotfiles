7294161ab9fc84816d7815ff582a5652902f79fb
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-04-16 19:56:26 -0400

    refactor(nix): move existing dotfiles to old_home

--- home.nix
+++ home.nix
@@ -4,10 +4,10 @@
   home.username = "anomalocaris";
   home.homeDirectory = "/home/anomalocaris";
   home.packages = with pkgs; [
-    git
     helix
     wezterm
   ];
+  imports = map (n: "./home/${n}") (builtins.attrNames (builtins.readDir ./home));
 
   home.persistence."/persist/home/anomalocaris" = {   
     allowOther = true;
@@ -32,7 +32,4 @@
 
   programs.home-manager.enable = true;
 
-  # Temporary
-  # Just the bare minimum to commit setup so far
-  home.file.".gitconfig".source = ./home/.gitconfig;
 }
