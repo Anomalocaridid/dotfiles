e352ace570b995c5161f26c72dbe47a1b75b85a3
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-04-25 15:28:07 -0400

    refactor(nix): tidy up home.nix

--- home.nix
+++ home.nix
@@ -1,42 +1,43 @@
-{ pkgs, ... }:
-
-{
-  home.username = "anomalocaris";
-  home.homeDirectory = "/home/anomalocaris";
-  home.packages = with pkgs; [
-    neofetch
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
-    firefox # fallback browser
-    nixpkgs-fmt
-    nil
-  ];
-
-
+{ pkgs, osConfig, ... }: {
+  # Import all files in ./home/
+  # Note: Will fail to build if non-nix files are present in ./home/
   imports = map (n: ./. + "/home/${n}") (builtins.attrNames (builtins.readDir ./home));
 
-  home.persistence."/persist/home/anomalocaris" = {
-    allowOther = true;
-    directories = [
-      # Default directories I care about
-      "Documents"
-      "Downloads"
-      "Music"
-      "Pictures"
-      "Videos"
-      # Other important stuff
-      "Sync" # Syncthing
-      "exercism" # Exercism
-      "Projects" # Misc. programming
-      "qmk_firmware" # QMK
-      ".ssh" # SSH key
-      # Caches I need to persist
-      ".cache/bat" # bat cache --build
+  home = rec {
+    username = "anomalocaris";
+    homeDirectory = "/home/${username}";
+    packages = with pkgs; [
+      neofetch
+      (nerdfonts.override { fonts = [ "FiraCode" ]; })
+      firefox # fallback browser
+      nixpkgs-fmt
+      nil
     ];
+
+    persistence."/persist/home/${username}" = {
+      allowOther = true;
+      directories = [
+        # Default directories I care about
+        "Documents"
+        "Downloads"
+        "Music"
+        "Pictures"
+        "Videos"
+        # Other important stuff
+        "Sync" # Syncthing
+        "exercism" # Exercism
+        "Projects" # Misc. programming
+        "qmk_firmware" # QMK
+        ".ssh" # SSH key
+        # Caches I need to persist
+        ".cache/bat" # bat cache --build
+      ];
+    };
+
+    # DON'T TOUCH
+    # Use system-level stateVersion
+    stateVersion = osConfig.system.stateVersion;
   };
 
-  # DON'T TOUCH
-  home.stateVersion = "22.11";
-
   programs.home-manager.enable = true;
-
 }
