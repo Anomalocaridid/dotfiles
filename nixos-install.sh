975dbfb9c25117a961a6ac5ff19f52048b1a9459
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-04-27 15:40:15 -0400

    refactor(nix): use --flake instead of cloning repo manually

--- nixos-install.sh
+++ nixos-install.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 # nixos-install.sh: install NixOS with my config
+
 # Sanity options for safety
-set -Eeuxo pipefail
+set -o errtrace \
+	-o errexit \
+	-o nounset \
+	-o xtrace \
+	-o pipefail
 
 # Config constants
 readonly USERNAME="anomalocaris"
+# NOTE: Remove branch argument before merge
 readonly CONFIG_REPO="Anomalocaridid/dotfiles"
+readonly FLAKE="github:$CONFIG_REPO/nixos#home-pc"
 readonly DISK="/dev/vda"
 readonly MEMORY="8G"
 
-# Clone dotfiles
-# NOTE: Remove branch argument before merge
-echo "Cloning config repo to temporary location"
-git clone -b nixos "https://github.com/$CONFIG_REPO.git" /tmp/dotfiles
+readonly MOUNT_DIR="/mnt"
+readonly PERSIST_DIR="/persist"
+readonly CONFIG_DIR="$MOUNT_DIR$PERSIST_DIR/etc/nixos"
 
 # Partition disk with disko
 echo "Partitioning disk with disko"
@@ -20,21 +26,18 @@ nix run github:nix-community/disko \
 	--extra-experimental-features nix-command \
 	--extra-experimental-features flakes \
 	-- \
-	--mode zap_create_mount "/tmp/dotfiles/disko-config.nix" \
+	--flake "$FLAKE" \
+	--mode zap_create_mount \
 	--argstr disk "$DISK" \
 	--argstr memory "$MEMORY" </dev/tty
 
-# Moving dotfiles to final location
-# NOTE: Change to location in /persist after initial config
-echo "Moving config repo to mounted disk"
-mkdir -p /mnt/etc
-mv /tmp/dotfiles /mnt/etc/nixos
+echo "Cloning config repo"
+# NOTE: Remove branch argument before merge
+git clone -b nixos "https://github.com/$CONFIG_REPO.git" "$CONFIG_DIR"
 
-# Generate hardware-config.nix
-# Disko will handle the filesystems and an existing configuration.nix won't be overridden
-# NOTE: Write to dotfiles' location in /persist after initial config
+# Will either leave config untouched or update hardware-configuration.nix and nothing else
 echo "Generating hardware config"
-nixos-generate-config --no-filesystems --root /mnt
+nixos-generate-config --no-filesystems --root "$MOUNT_DIR" --dir "$PERSIST_DIR"
 
 # Generate password hashes
 echo "Setting password (trace disabled)"
@@ -59,7 +62,7 @@ while true; do
 	fi
 done
 
-readonly PASSWORD_HASH_DIR="/mnt/persist/passwords"
+readonly PASSWORD_HASH_DIR="$MOUNT_DIR$PERSIST_DIR/passwords"
 mkdir -p $PASSWORD_HASH_DIR
 # Password may be the same, but calling mkpasswd twice ensures each hash has a different salt
 mkpasswd "$password" --stdin --method=yescrypt >"$PASSWORD_HASH_DIR/root"
@@ -68,7 +71,6 @@ mkpasswd "$password" --stdin --method=yescrypt >"$PASSWORD_HASH_DIR/$USERNAME"
 set -x
 
 # Install NixOS
-# NOTE: Uncomment after initial config
-# echo "Installing NixOS and rebooting"
-# nixos-install --no-root-passwd
-# reboot
+echo "Installing NixOS and rebooting"
+nixos-install --flake "git+file://$CONFIG_DIR#home-pc" --no-root-passwd
+#reboot
