8488cb3224912a4da92a0ce79f58db81863d6131
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-07-16 15:07:57 -0400

    fix(pkgs): update advcpmv-coreutils

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -45,24 +45,15 @@ final: prev: {
 
   # Custom-written packages
   custom = {
-    cyberre-grub-theme = final.pkgs.callPackage ./cyberre-grub-theme { };
-    gtk-materia-cyberpunk-neon = final.pkgs.callPackage ./gtk-materia-cyberpunk-neon { };
-    candy-icons = final.pkgs.callPackage ./candy-icons { };
-    breeze-hacked-cursor = final.pkgs.callPackage ./breeze-hacked-cursor { };
+    cyberre-grub-theme = final.callPackage ./cyberre-grub-theme { };
+    gtk-materia-cyberpunk-neon = final.callPackage ./gtk-materia-cyberpunk-neon { };
+    candy-icons = final.callPackage ./candy-icons { };
+    breeze-hacked-cursor = final.callPackage ./breeze-hacked-cursor { };
     # cp and mv with progress bars
     advcpmv-coreutils = prev.coreutils.overrideAttrs (oldAttrs: rec {
-      version = "9.2";
-      patch-version = "0.9";
-
-      # Necessary because advcpmv can lag behind coreutils
-      src = final.fetchurl {
-        url = "mirror://gnu/coreutils/coreutils-${version}.tar.xz";
-        hash = "sha256-aIX/R7nNshHeR9NowXhT9Abar5ixSKrs3xDeKcwEsLM=";
-      };
-
       advcpmv-patch = final.fetchpatch {
-        url = "https://raw.githubusercontent.com/jarun/advcpmv/master/advcpmv-${patch-version}-${version}.patch";
-        hash = "sha256-l2WGdEKd8hxHUL3IrK+8jFuc0eLme0387WB7ObWWwIA=";
+        url = "https://raw.githubusercontent.com/jarun/advcpmv/master/advcpmv-0.9-9.3.patch";
+        hash = "sha256-I25F7uHESUsMDZFYTv8/56eR8QwelIPpABRXTgvszQI=";
       };
 
       patches = (oldAttrs.patches or [ ]) ++ [ advcpmv-patch ];
