809ab5a61ecb15aa1e0c7e71fbac32ef941f3599
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-07-30 17:23:52 -0400

    feat(gaming): add parsec

--- home/gaming.nix
+++ home/gaming.nix
@@ -3,6 +3,7 @@
     gamescope # Used by Lutris for control over game resolution
     lutris
     packwiz # minecraft modpack creator
+    parsec-bin # Online multiplayer for local multiplayer games
     prismlauncher-qt5 # Non-qt5 version does not work as well with theme
     pysolfc
     sgtpuzzles
