485e547b314bdefc077330675273fcd014c7413a
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-08-14 14:12:04 -0400

    refactor(hyprland): use settings instead of extraConfig where possible

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -6,177 +6,186 @@
   ];
   wayland.windowManager.hyprland = {
     enable = true;
-    extraConfig =
+    settings =
       let
         pink = "ea00d9";
         purple = "711c91";
         darkBlue = "000b1e";
         lightBlue = "133e7c";
-        cyan = "0abdc6";
-        green = "00ff00";
-        orange = "f57800";
-        red = "ff00ff";
+        # cyan = "0abdc6";
+        # green = "00ff00";
+        # orange = "f57800";
+        # red = "ff00ff";
       in
       #hypr
-      ''
+      {
         # Execute your favorite apps at launch
-        # TODO: steam
-        # Set up live wallpaper
-        # https://moewalls.com/landscape/synthwave-city-live-wallpaper/
-        exec-once = mpvpaper -o "no-audio loop" HDMI-A-1 "/etc/nixos/assets/wallpaper.mp4"
-        exec-once = eww open bar
-        exec-once = hyprland-autoname-workspaces
-        exec-once = webcord
+        exec-once = [
+          # Set up live wallpaper
+          # https://moewalls.com/landscape/synthwave-city-live-wallpaper/
+          "mpvpaper -o 'no-audio loop' HDMI-A-1 '/etc/nixos/assets/wallpaper.mp4'"
+          "eww open bar"
+          "hyprland-autoname-workspaces"
+          "webcord"
+          "steam -silent"
+        ];
 
-        general {
-            gaps_in = 5
-            gaps_out = 20
-            border_size = 2
-            col.active_border = rgba(${pink}ee) rgba(${purple}ee) 45deg
-            col.inactive_border = rgba(${darkBlue}aa) rgba(${lightBlue}aa) 45deg
-            # col.group_border = rgba(${darkBlue}aa) rgba(${lightBlue}ee) 45deg
-            # col.group_border_active = rgba(${cyan}ee) rgba(${green}ee) 45deg
-            # col.group_border_locked = rgba(${darkBlue}aa) rgba(${lightBlue}ee) 45deg
-            # col.group_border_locked_active = rgba(${orange}ee) rgba(${red}ee) 45deg
+        general = {
+          gaps_in = 5;
+          gaps_out = 20;
+          border_size = 2;
+          "col.active_border" = "rgba(${pink}ee) rgba(${purple}ee) 45deg";
+          "col.inactive_border" = "rgba(${darkBlue}aa) rgba(${lightBlue}aa) 45deg";
+          # "col.group_border" = "rgba(${darkBlue}aa) rgba(${lightBlue}ee) 45deg";
+          # "col.group_border_active" = "rgba(${cyan}ee) rgba(${green}ee) 45deg";
+          # "col.group_border_locked" = "rgba(${darkBlue}aa) rgba(${lightBlue}ee) 45deg";
+          # "col.group_border_locked_active" = "rgba(${orange}ee) rgba(${red}ee) 45deg";
 
-            layout = dwindle
-        }
+          layout = "dwindle";
+        };
 
-        decoration {
-            # See https://wiki.hyprland.org/Configuring/Variables/ for more
-            rounding = 10
+        decoration = {
+          # See https://wiki.hyprland.org/Configuring/Variables/ for more
+          rounding = 10;
 
-            # blur {
-            #   enabled = true
-            #   size = 3
-            #   passes = 1
-            # }
+          # blur = {
+          #   enabled = true;
+          #   size = 3;
+          #   passes = 1;
+          # }
 
-            blur = true
-            blur_size = 3
-            blur_passes = 1
+          blur = true;
+          blur_size = 3;
+          blur_passes = 1;
 
-            drop_shadow = true
-            shadow_range = 4
-            shadow_render_power = 3
-            col.shadow = rgba(${darkBlue}ee)
-        }
+          drop_shadow = true;
+          shadow_range = 4;
+          shadow_render_power = 3;
+          "col.shadow" = "rgba(${darkBlue}ee)";
+        };
 
-        animations {
-            enabled = true
+        animations = {
+          enabled = true;
 
-            # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-            bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+          # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+          bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
 
-            animation = windows, 1, 7, myBezier
-            animation = windowsOut, 1, 7, default, popin 80%
-            animation = border, 1, 10, default
-            animation = borderangle, 1, 8, default
-            animation = fade, 1, 7, default
-            animation = workspaces, 1, 6, default
-        }
+          animation = [
+            "windows, 1, 7, myBezier"
+            "windowsOut, 1, 7, default, popin 80%"
+            "border, 1, 10, default"
+            "borderangle, 1, 8, default"
+            "fade, 1, 7, default"
+            "workspaces, 1, 6, default"
+          ];
+        };
 
-        dwindle {
-            # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-            pseudotile = true # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-            preserve_split = true # you probably want this
-        }
+        dwindle = {
+          # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+          pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+          preserve_split = true; # you probably want this
+        };
 
-        master {
-            # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-            new_is_master = true
-        }
+        # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+        master.new_is_master = true;
 
-        gestures {
-            # See https://wiki.hyprland.org/Configuring/Variables/ for more
-            workspace_swipe = false
-        }
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        gestures.workspace_swipe = false;
 
-        misc {
-          enable_swallow = true
-          swallow_regex = ^(org.wezfurlong.wezterm)$
-          # groupbar_text_color = rgb(${cyan})
-        }
-        
+        misc = {
+          enable_swallow = true;
+          swallow_regex = "^(org.wezfurlong.wezterm)$";
+          # groupbar_text_color = "rgb(${cyan})";
+        };
+
         # Window rules
-        windowrulev2 = float, class:^(wlogout|pavucontrol|nmtui)$
-        windowrulev2 = workspace 1, class:^(lutris)$
-        windowrulev2 = workspace 2, class:^(nyxt)$
-        windowrulev2 = workspace 3, class:^(nnn)$
-        windowrulev2 = workspace 4 silent, class:^(WebCord)$
-        windowrulev2 = workspace 5, title:^(Spotify)$
-        # Inhibit idle on fullscreen programs where keyboard/mouse may not be used for a while
-        windowrulev2 = idleinhibit fullscreen, class:^(FreeTube)$
-        
-        $mainMod = SUPER
-        
-        $shellExec = zsh -c
-        $opener = handlr launch
-        $term = $opener x-scheme-handler/terminal --
-        # Launch nnn in a shell so it gets the necessary variables and a custom class
-        # FIXME: Cannot open preview-tui if nnn is opened with this
-        $fileMan = $term --class=nnn -- $shellExec "$opener inode/directory"
+        windowrulev2 = [
+          "float, class:^(wlogout|pavucontrol|nmtui)$"
+          "workspace 1, class:^(lutris)$"
+          "workspace 2, class:^(nyxt)$"
+          "workspace 3, class:^(nnn)$"
+          "workspace 4 silent, class:^(WebCord)$"
+          "workspace 5, title:^(Spotify)$"
+          # Inhibit idle on fullscreen programs where keyboard/mouse may not be used for a while
+          "idleinhibit fullscreen, class:^(FreeTube)$"
+        ];
 
-        # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-        bind = $mainMod, Return, exec, $term
-        bind = $mainMod, Q, killactive,
-        bind = $mainMod SHIFT, Q, exec, wlogout
-        bind = $mainMod, N, execr, $fileMan
-        bind = $mainMod, V, togglefloating,
-        bind = $mainMod, R, exec, wofi --show drun
-        bind = $mainMod, P, pseudo, # dwindle
-        bind = $mainMod, S, togglesplit, # dwindle
-        bind = $mainMod, D, exec, hyprctl keyword general:layout dwindle
-        bind = $mainMod, M, exec, hyprctl keyword general:layout master
-        bind = $mainMod, O, exec, nyxt
-        # bind = $mainMod, G, togglegroup
-        # bind = $mainMod SHIFT, G, lockgroups, toggle
+        "$mainMod" = "SUPER";
+
+        "$shellExec" = "zsh -c";
+        "$opener" = "handlr launch";
+        "$term" = "$opener x-scheme-handler/terminal --";
+        # Launch nnn in a shell so it gets the necessary variables and a custom class
+        "$fileMan" = "$term --class=nnn -- $shellExec '$opener inode/directory'";
 
         # Vim-style homerow direction keys
-        $left = h
-        $down = j
-        $up = k
-        $right = l
+        "$left" = "h";
+        "$down" = "j";
+        "$up" = "k";
+        "$right" = "l";
 
-        # Move focus with mainMod + direction keys
-        # Swap active window with an adjacent window with mainMod + SHIFT + direction keys
-        ${lib.strings.concatMapStrings (key:
-            let
-              dir = (builtins.elemAt (lib.strings.stringToCharacters key) 1);
-            in
-            #hypr
-            ''
-              bind = $mainMod, ${key}, movefocus, ${dir}
-              bind = $mainMod SHIFT, ${key}, swapwindow, ${dir}
-            ''
-          ) [ "$left" "$down" "$up" "$right" ]};
+        # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
+        bind = lib.flatten [
+          "$mainMod, Return, exec, $term"
+          "$mainMod, Q, killactive,"
+          "$mainMod SHIFT, Q, exec, wlogout"
+          "$mainMod, N, execr, $fileMan"
+          "$mainMod, V, togglefloating,"
+          "$mainMod, R, exec, wofi --show drun"
+          "$mainMod, P, pseudo, # dwindle"
+          "$mainMod, S, togglesplit, # dwindle"
+          "$mainMod, D, exec, hyprctl keyword general:layout dwindle"
+          "$mainMod, M, exec, hyprctl keyword general:layout master"
+          "$mainMod, O, exec, nyxt"
+          # "$mainMod, G, togglegroup"
+          # "$mainMod SHIFT, G, lockgroups, toggle"
 
-        # Switch workspaces with mainMod + [0-9]
-        # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        ${builtins.concatStringsSep "\n" (builtins.genList (x:
-            let
-              ws = x + 1;
-              key = toString (lib.trivial.mod ws 10);
-            in 
-            #hypr
-            ''
-              bind = $mainMod, ${key}, workspace, ${toString ws}
-              bind = $mainMod SHIFT, ${key}, movetoworkspace, ${toString ws}
-            ''
-          )
-          10)}
+          # Move focus with mainMod + direction keys
+          # Swap active window with an adjacent window with mainMod + SHIFT + direction keys
+          (builtins.map
+            (key:
+              let
+                dir = (builtins.elemAt (lib.strings.stringToCharacters key) 1);
+              in
+              [
+                "$mainMod, ${key}, movefocus, ${dir}"
+                "$mainMod SHIFT, ${key}, swapwindow, ${dir}"
+              ]
+            )
+            [ "$left" "$down" "$up" "$right" ])
 
-        # Scroll through existing workspaces with mainMod + scroll
-        bind = $mainMod, mouse_down, workspace, e+1
-        bind = $mainMod, mouse_up, workspace, e-1
+          # Switch workspaces with mainMod + [0-9]
+          # Move active window to a workspace with mainMod + SHIFT + [0-9]
+          (builtins.genList
+            (x:
+              let
+                ws = x + 1;
+                key = toString (lib.trivial.mod ws 10);
+              in
+              [
+                "$mainMod, ${key}, workspace, ${toString ws}"
+                "$mainMod SHIFT, ${key}, movetoworkspace, ${toString ws}"
+              ]
+            )
+            10)
 
-        $LMB = mouse:272
-        $RMB = mouse:275
+          # Scroll through existing workspaces with mainMod + scroll
+          "$mainMod, mouse_down, workspace, e+1"
+          "$mainMod, mouse_up, workspace, e-1"
+        ];
 
-        # Move/resize windows with mainMod + LMB/RMB and dragging
-        bindm = $mainMod, $LMB, movewindow
-        bindm = $mainMod, $RMB, resizewindow
+        "$LMB" = "mouse:272";
+        "$RMB" = "mouse:275";
 
+        bindm = [
+          # Move/resize windows with mainMod + LMB/RMB and dragging
+          "$mainMod, $LMB, movewindow"
+          "$mainMod, $RMB, resizewindow"
+        ];
+
+      };
+    extraConfig = #hypr
+      ''
         # Resize submap
         bind = $mainMod ALT,R,submap,resize
         submap = resize
@@ -186,9 +195,9 @@
         binde = ,$up,resizeactive,0 -10
         binde = ,$down,resizeactive,0 10
 
-        bind = ,escape,submap,reset 
+        bind = ,escape,submap,reset
 
-        submap=reset
+        submap = reset
       '';
   };
 
