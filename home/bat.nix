ade7841a5ba5fe86660bff0aef6d801d1f82c42b
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-09-11 22:39:37 -0400

    fix(bat): fix less not displaying nerd font icons

--- home/bat.nix
+++ home/bat.nix
@@ -14,6 +14,11 @@
       batpipe
     ];
   };
+
+  # Designate Unicode Private Use Areas as printable characters
+  # Needed for Nerd Fonts icons to display properly
+  home.sessionVariables.LESSUTFCHARDEF = "E000-F8FF:p,F0000-FFFFD:p,100000-10FFFD:p";
+
   systemd.user.services.bat-cache = {
     Unit.Description = "Build and update bat cache";
     Service = {
