e2cd203151fa618b670f91e4a3bbce2d52bb61db
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-12-28 22:17:16 -0500

    refactor(wezterm): use lib.generators.toLua

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -1,60 +1,40 @@
-{ config, ... }: {
+{ config, lib, ... }: {
   programs.wezterm = {
     enable = true;
     extraConfig =
       let
         fonts = config.stylix.fonts;
+        luaFormat = lib.generators.toLua { };
+        lua = luaFormat {
+          hide_tab_bar_if_only_one_tab = true;
+          window_background_opacity = 0.90;
+          exit_behavior = "Close";
+          check_for_updates = false;
+          enable_wayland = true;
+          enable_kitty_graphics = true;
+          window_close_confirmation = "NeverPrompt";
+
+          font_size = fonts.sizes.terminal;
+          font = lib.generators.mkLuaInline #lua
+            ''
+                wezterm.font${ luaFormat {
+              		family = fonts.monospace.name;
+              		harfbuzz_features = [
+              			"ss09" # >>= <<= ||= |=
+              			"cv25" # .-
+              			"cv26" # :-
+              			"cv32" # .=
+              			"cv27" # []
+              			"cv28" # {. .}
+                    "ss06" # \\
+              			"ss07" # =~ !~
+              		];
+                } }
+            '';
+          color_scheme = "catppuccin-${config.catppuccin.flavour}";
+        };
+
       in
-      # lua
-      ''
-        return {
-        	hide_tab_bar_if_only_one_tab = true,
-        	window_background_opacity = 0.90,
-        	exit_behavior = "Close",
-        	check_for_updates = false,
-        	enable_wayland = true,
-        	enable_kitty_graphics = true,
-          window_close_confirmation = "NeverPrompt",
-
-        	font_size = ${toString fonts.sizes.terminal},
-        	font = wezterm.font {
-        		family = "${fonts.monospace.name}",
-        		harfbuzz_features = {
-        			"ss09", -- >>= <<= ||= |=
-        			"cv25", -- .-
-        			"cv26", -- :-
-        			"cv32", -- .=
-        			"cv27", -- []
-        			"cv28", -- {. .}
-        			"ss07", -- =~ !~
-        		},
-        	},
-
-        	color_scheme = "catppuccin-mocha",
-
-          -- TODO:
-        	--colors = {
-        	--	tab_bar =  {
-        	--		background = "#000b1e",
-
-        	--		active_tab = {
-        	--			bg_color = "#005faf",
-        	--			fg_color = "#0abdc6",
-        	--			intensity = "Bold",
-        	--		},
-
-        	--		inactive_tab = {
-        	--			bg_color = "#000b1e",
-          --      fg_color = "#0abdc6",
-        	--		},
-
-        	--		inactive_tab_hover = {
-        	--			bg_color = "#000b1e",
-        	--			fg_color = "#711c91",    			
-          --    },
-        	--	},
-        	--},
-        }
-      '';
+      "return ${lua}";
   };
 }
