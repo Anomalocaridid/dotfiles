37aaaa6b5c42f5e610cd867776e4aa753ab32e50
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-01-30 15:50:23 -0500

    feat(development): add ruby
    feat(development): add ruby

---
+++ home/development/ruby.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    ruby
+    rubyPackages.minitest
+  ];
+
+  programs.helix = {
+    languages = {
+      language-server.solargraph.config = {
+        diagnostics = true;
+        formatting = true;
+      };
+      language = [
+        {
+          name = "ruby";
+          auto-format = true;
+        }
+      ];
+    };
+    extraPackages = with pkgs; [
+      rubyPackages.solargraph
+    ];
+  };
+}
