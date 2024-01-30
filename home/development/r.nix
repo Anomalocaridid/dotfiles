0fc785e5df2042293b17887d4590199edbb58703
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-01-30 16:56:02 -0500

    feat(development): add r

---
+++ home/development/r.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    (rWrapper.override {
+      packages = with rPackages; [
+        testthat # Needed for exercism tests
+      ];
+    })
+  ];
+
+  programs.helix = {
+    extraPackages = with pkgs; [
+      (rWrapper.override {
+        packages = with rPackages; [
+          languageserver
+          lintr
+        ];
+      })
+    ];
+    languages.language = [{
+      name = "r";
+      auto-format = true;
+    }];
+  };
+}
