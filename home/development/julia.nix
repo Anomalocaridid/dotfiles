38e6399c9a421849918afc93cac27dec7c969d92
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-02-06 15:16:41 -0500

    fix(helix): auto-format julia files

--- home/development/julia.nix
+++ home/development/julia.nix
@@ -7,4 +7,11 @@
       ".julia/config/startup.jl".source = ../.julia/config/startup.jl;
     };
   };
+
+  programs.helix.languages.language = [
+    {
+      name = "julia";
+      auto-format = true;
+    }
+  ];
 }
