75d4bfa852a4ab6e2f99884a03d136959d06677d
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-02-17 19:56:15 -0500

    feat(development): add crystal

---
+++ home/development/crystal.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    crystal
+  ];
+
+  programs.helix = {
+    extraPackages = with pkgs; [
+      crystalline
+    ];
+    languages.language = [
+      {
+        name = "crystal";
+        auto-format = true;
+      }
+    ];
+  };
+}
