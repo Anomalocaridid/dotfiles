3d0082060afbf6dc832a8e5dc227e9242b9ac145
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-04-07 13:46:44 -0400

    refactor(theming): use catppuccin/nix for delta

--- home/git.nix
+++ home/git.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ pkgs, ... }: {
   programs.git = {
     enable = true;
     userEmail = "29845794+Anomalocaridid@users.noreply.github.com";
@@ -8,42 +8,10 @@
 
     delta = {
       enable = true;
+      catppuccin.enable = true;
       options = {
         navigate = true;
-        features = "catppuccin-${config.catppuccin.flavour}";
-        # side-by-side = true;
         interactive.keep-plus-minus-markers = false;
-        "catppuccin-${config.catppuccin.flavour}" =
-          let
-            palette = pkgs.custom.catppuccin-palette.${config.catppuccin.flavour};
-            red20Percent = "483346";
-            red60Percent = "9d5477";
-            green20Percent = "394545";
-            green60Percent = "6f9473";
-          in
-          {
-            commit-decoration-style = "box ul";
-            dark = true;
-            file-decoration-style = "#${palette.text.hex}";
-            file-style = "#${palette.text.hex}";
-            hunk-header-decoration-style = "box ul";
-            hunk-header-file-style = "bold";
-            hunk-header-line-number-style = "bold #${palette.subtext0.hex}";
-            hunk-header-style = "file line-number syntax";
-            line-numbers = true;
-            line-numbers-left-style = "#${palette.overlay0.hex}";
-            line-numbers-minus-style = "bold #${palette.red.hex}";
-            line-numbers-plus-style = "bold #${palette.green.hex}";
-            line-numbers-right-style = "#${palette.overlay0.hex}";
-            line-numbers-zero-style = "#${palette.overlay0.hex}";
-            # Not exactly standards-conformant,
-            # But idk how to make it look good otherwise
-            minus-emph-style = "bold syntax #${red60Percent}";
-            minus-style = "bold syntax #${red20Percent}";
-            plus-emph-style = "bold syntax #${green60Percent}";
-            plus-style = "bold syntax #${green20Percent}";
-            syntax-theme = "Catppuccin-${config.catppuccin.flavour}";
-          };
       };
     };
 
@@ -67,9 +35,8 @@
 
   home.sessionVariables = {
     # Ensure bat's line numbers don't show up and mess things up
-    # Also ensure --side-by-side is only used for git diffs
-    # GIT_PAGER = "PAGER='bat --plain' delta --side-by-side";
-    GIT_PAGER = "PAGER='bat --plain' delta";
+    DELTA_PAGER = "bat --plain";
+    # Ensure --side-by-side is only used for git diffs
     DELTA_FEATURES = "+side-by-side";
   };
 }
