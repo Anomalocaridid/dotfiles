26635df36173897acb0c2e7b4604d2c1262d3cc8
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-04-07 13:46:30 -0400

    refactor(theming): use catppuccin/nix for imv

--- home/imv.nix
+++ home/imv.nix
@@ -1,19 +1,16 @@
-{ config, pkgs, ... }: {
+{ config, ... }: {
   programs.imv =
     let
       fonts = config.stylix.fonts;
-      palette = pkgs.custom.catppuccin-palette.${config.catppuccin.flavour};
     in
     {
       enable = true;
+      catppuccin.enable = true;
       settings = {
         options = {
-          background = "${palette.base.hex}";
           overlay = true;
           overlay_font = "${fonts.sansSerif.name}:${toString fonts.sizes.applications}";
           overlay_text = "$imv_current_file";
-          overlay_text_color = "${palette.text.hex}";
-          overlay_background_color = "${palette.mantle.hex}";
           title_text = "imv - \${imv_current_file##*/}";
         };
       };
