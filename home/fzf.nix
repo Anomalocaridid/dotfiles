a31ea1548c34d4006587dac1c478ea6f8e382d03
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-04-18 18:14:48 -0400

    refactor(theming): use catppuccin/nix for fzf

--- home/fzf.nix
+++ home/fzf.nix
@@ -1,24 +1,7 @@
-{ config, pkgs, ... }: {
+{ ... }: {
   programs.fzf = {
     enable = true;
-    colors =
-      let
-        palette = pkgs.custom.catppuccin-palette.${config.catppuccin.flavour};
-      in
-      {
-        "bg+" = "#${palette.surface0.hex}";
-        bg = "#${palette.base.hex}";
-        spinner = "#${palette.rosewater.hex}";
-        hl = "#${palette.red.hex}";
-        fg = "#${palette.text.hex}";
-        header = "#${palette.red.hex}";
-        info = "#${palette.mauve.hex}";
-        pointer = "#${palette.rosewater.hex}";
-        marker = "#${palette.rosewater.hex}";
-        "fg+" = "#${palette.text.hex}";
-        prompt = "#${palette.mauve.hex}";
-        "hl+" = "#${palette.red.hex}";
-      };
+    catppuccin.enable = true;
     defaultOptions = [ ''--preview 'bat --plain --color=always "{}"' '' ];
   };
 }
