b587ea9263af9a886c85816e0fae9bbd9121d090
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-05-19 13:22:08 -0400

    fix(lockman): re-add pipes-rs config

--- home/hyprlock.nix
+++ home/hyprlock.nix
@@ -94,4 +94,27 @@
         ];
       };
     };
+
+  # Screensaver config
+  xdg.configFile."pipes-rs/config.toml".source =
+    let
+      tomlFormat = pkgs.formats.toml { };
+    in
+    tomlFormat.generate "pipes-rs-config" {
+      # bold = true;
+      color_mode = "rgb"; # ansi, rgb or none
+      # palette = "default"; # default, darker, pastel or matrix
+      rainbow = 1; # 0-255
+      # delay_ms = 20;
+      # inherit_style = false;
+      kinds = [
+        "heavy"
+        "light"
+        "curved"
+        "outline"
+      ]; # heavy, light, curved, knobby, emoji, outline, dots, blocks, sus
+      num_pipes = 2;
+      # reset_threshold = 0.5; # 0.0–1.0
+      # turn_chance = 0.15; # 0.0–1.0
+    };
 }
