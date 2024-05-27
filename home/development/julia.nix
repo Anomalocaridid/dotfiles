f86c7680534f9642798948d21b23046b03fcd133
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-05-27 16:47:38 -0400

    fix(development): remove LanguageServer from julia

--- home/development/julia.nix
+++ home/development/julia.nix
@@ -1,12 +1,14 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    (julia.withPackages [
-      # Don't separate LanguageServer into helix's extraPackages
-      # because that would require compiling Julia's depot twice
-      # and this is not a separate program anyways
-      "LanguageServer"
-    ])
+    julia
+    # Disabled because of build errors
+    # (julia.withPackages [
+    #   # Don't separate LanguageServer into helix's extraPackages
+    #   # because that would require compiling Julia's depot twice
+    #   # and this is not a separate program anyways
+    #   "LanguageServer"
+    # ])
   ];
 
   programs.helix.languages.language = [
