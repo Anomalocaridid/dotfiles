545c0b8facde05c16064995d768babda8b52e660
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-06-26 13:53:03 -0400

    fix(development): re-add julia language server

--- home/development/julia.nix
+++ home/development/julia.nix
@@ -1,14 +1,12 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    julia
-    # Disabled because of build errors
-    # (julia.withPackages [
-    #   # Don't separate LanguageServer into helix's extraPackages
-    #   # because that would require compiling Julia's depot twice
-    #   # and this is not a separate program anyways
-    #   "LanguageServer"
-    # ])
+    (julia.withPackages [
+      # Don't separate LanguageServer into helix's extraPackages
+      # because that would require compiling Julia's depot twice
+      # and this is not a separate program anyways
+      "LanguageServer"
+    ])
   ];
 
   programs.helix.languages.language = [
