ade87b9149126fcfbf58a2c9b70cc947aaadafd1
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-07-31 23:00:02 -0400

    fix(fuzzel): fix theme setting

--- home/fuzzel.nix
+++ home/fuzzel.nix
@@ -1,28 +1,16 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, pkgs, ... }:
 {
   programs.fuzzel =
     let
       fonts = config.stylix.fonts;
-      themeFile = pkgs.sources.catppuccin-fuzzel + "/themes/${config.catppuccin.flavor}.ini";
-      theme =
-        let
-          inherit (builtins) fromJSON readFile;
-
-          json = pkgs.runCommand "converted.json" { } ''
-            ${lib.getExe pkgs.jc} --ini < ${themeFile} > $out;
-          '';
-        in
-        fromJSON (readFile json);
     in
     {
       enable = true;
-      settings = theme // {
+      settings = {
         main = {
+          include =
+            pkgs.sources.catppuccin-fuzzel
+            + "/themes/${config.catppuccin.flavor}/${config.catppuccin.accent}.ini";
           font = "${fonts.monospace.name}:size=${toString fonts.sizes.applications}";
           dpi-aware = "no"; # appears really small otherwise
           icon-theme = config.gtk.iconTheme.name;
