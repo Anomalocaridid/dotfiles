934d63d5d1c97c6c45baef68800ec6cda41ed55a
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-08-11 21:26:05 -0400

    feat(spicetify): use workaround to declaratively set accent color

--- home-modules/spicetify.nix
+++ home-modules/spicetify.nix
@@ -10,7 +10,22 @@ in
 {
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.catppuccin;
+    theme = spicePkgs.themes.catppuccin // {
+      requiredExtensions =
+        let
+          name = "accent.js";
+        in
+        [
+          {
+            src =
+              pkgs.writeTextDir name # javascript
+                ''
+                  localStorage.setItem("catppuccin-accentColor", "${config.catppuccin.accent}");
+                '';
+            inherit name;
+          }
+        ];
+    };
     colorScheme = config.catppuccin.flavor;
     enabledExtensions = with spicePkgs.extensions; [
       # Official extensions
