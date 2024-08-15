f487de1f89c2f7bbe264ec3dab90ecb9ad5f35ca
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-08-15 08:38:02 -0400

    feat(lazygit): use `delta` as the pager

--- home-modules/git.nix
+++ home-modules/git.nix
@@ -27,13 +27,25 @@
       theme.nerdFontsVersion = 3;
       update.method = false;
       disableStartupPopups = true;
+      git =
+        let
+          logCmd = "git log --color=always";
+        in
+        {
+          paging = {
+            colorArg = "always";
+            pager = ''DELTA_FEATURES="+" delta --paging=never'';
+          };
+          branchLogCmd = "${logCmd} {{branchName}}";
+          allBranchesLogCmds = [ "${logCmd} --all" ];
+        };
     };
   };
 
   home.sessionVariables = {
     # Ensure bat's line numbers don't show up and mess things up
     DELTA_PAGER = "bat --plain";
-    # Ensure --side-by-side is only used for git diffs
+    # Ensure --side-by-side is only used for `git diff`
     DELTA_FEATURES = "+side-by-side";
   };
 }
