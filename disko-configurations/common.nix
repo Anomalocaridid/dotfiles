eeca06b34d85d88ead0030431d60272120d66ab4
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-08-19 19:15:54 -0400

    fix(disko): update to use `gpt` instead of `table`

--- disko-configurations/common.nix
+++ disko-configurations/common.nix
@@ -1,98 +1,86 @@
-{ disk, memory, ... }:
+{ disk, memory }:
 {
   disko.devices = {
-    disk = {
-      vda = {
-        type = "disk";
-        device = disk;
-        content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "ESP";
-              start = "1MiB";
-              end = "512MiB";
-              bootable = true;
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "defaults" ];
-              };
-            }
-            {
-              name = "luks";
-              start = "512MiB";
-              end = "100%";
-              content = {
-                type = "luks";
-                name = "crypted";
-                content = {
-                  type = "lvm_pv";
-                  vg = "pool";
-                };
-              };
-            }
-          ];
-        };
-      };
-    };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          # LVs are created in alphabetic order for some reason
-          # Prefixing them with a letter like this ensures desired creation order
-          a_swap = {
-            size = memory;
+    disk.main = {
+      type = "disk";
+      device = disk;
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
             content = {
-              type = "swap";
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "defaults" ];
             };
           };
-          z_persist = {
-            size = "100%FREE";
+          luks = {
+            size = "100%";
             content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ]; # Override existing partition
-              # Subvolumes must set a mountpoint in order to be mounted 
-              # unless its parent is mounted
-              subvolumes =
-                let
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                    "nodiratime"
-                    "discard"
-                  ];
-                in
-                {
-                  "/nix" = {
-                    inherit mountOptions;
-                    mountpoint = "/nix";
-                  };
-                  "/persist" = {
-                    inherit mountOptions;
-                    mountpoint = "/persist";
-                  };
-                  "/log" = {
-                    inherit mountOptions;
-                    mountpoint = "/var/log";
-                  };
-                };
+              type = "luks";
+              name = "crypted";
+              settings.allowDiscards = true;
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
             };
           };
         };
       };
     };
-    nodev = {
-      "/" = {
-        fsType = "tmpfs";
-        mountOptions = [
-          "defaults"
-          "mode=755"
-        ];
+    lvm_vg.pool = {
+      type = "lvm_vg";
+      lvs = {
+        swap = {
+          size = memory;
+          content.type = "swap";
+        };
+        persist = {
+          # Uses different format for specifying size
+          # Based on `lvcreate` arguments
+          size = "100%FREE";
+          content = {
+            type = "btrfs";
+            extraArgs = [ "-f" ]; # Override existing partition
+            # Subvolumes must set a mountpoint in order to be mounted 
+            # unless its parent is mounted
+            subvolumes =
+              let
+                mountOptions = [
+                  "compress=zstd"
+                  "noatime"
+                  "nodiratime"
+                  "discard"
+                ];
+              in
+              {
+                "/nix" = {
+                  inherit mountOptions;
+                  mountpoint = "/nix";
+                };
+                "/persist" = {
+                  inherit mountOptions;
+                  mountpoint = "/persist";
+                };
+                "/log" = {
+                  inherit mountOptions;
+                  mountpoint = "/var/log";
+                };
+              };
+          };
+        };
       };
+    };
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "defaults"
+        "mode=755"
+      ];
     };
   };
 }
