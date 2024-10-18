e6529b77e14000c1b72211d92a5d36c6fa60b3af
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-10-18 16:55:25 -0400

    refactor(mimeapps): use handlr-regex's wildcards to be less redundant

--- home-modules/mimeapps.nix
+++ home-modules/mimeapps.nix
@@ -8,31 +8,18 @@
           editor = "Helix.desktop";
           browser = "firefox.desktop";
           pdfViewer = "org.pwmt.zathura-pdf-mupdf.desktop";
-          wordProcessor = "writer.desktop";
-          spreadsheet = "calc.desktop";
-          presentation = "impress.desktop";
+          officeSuite = "startcenter.desktop";
         in
         {
           "application/epub+zip" = pdfViewer;
-          "application/msword" = wordProcessor;
+          "application/msword" = "writer.desktop";
           "application/oxps" = pdfViewer;
           "application/pdf" = pdfViewer;
           "application/toml" = editor;
-          "application/vnd.ms-excel" = spreadsheet;
-          "application/vnd.ms-powerpoint" = presentation;
-          "application/vnd.oasis.opendocument.database" = "base.desktop";
-          "application/vnd.oasis.opendocument.formula" = "math.desktop";
-          "application/vnd.oasis.opendocument.graphics" = "draw.desktop";
-          "application/vnd.oasis.opendocument.presentation" = presentation;
-          "application/vnd.oasis.opendocument.presentation-template" = presentation;
-          "application/vnd.oasis.opendocument.spreadsheet" = spreadsheet;
-          "application/vnd.oasis.opendocument.spreadsheet-flat-xml" = spreadsheet;
-          "application/vnd.oasis.opendocument.text" = wordProcessor;
-          "application/vnd.oasis.opendocument.text-template" = wordProcessor;
-          "application/vnd.openxmlformats-officedocument.presentationml.presentation" = presentation;
-          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = spreadsheet;
-          "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = wordProcessor;
-          "application/vnd.openxmlformats-officedocument.wordprocessingml.template" = wordProcessor;
+          "application/vnd.ms-excel" = "calc.desktop";
+          "application/vnd.ms-powerpoint" = "impress.desktop";
+          "application/vnd.oasis.opendocument.*" = officeSuite;
+          "application/vnd.openxmlformats-officedocument.*" = officeSuite;
           "application/x-yaml" = editor;
           "audio/*" = "org.strawberrymusicplayer.strawberry.desktop";
           "image/*" = "imv.desktop";
