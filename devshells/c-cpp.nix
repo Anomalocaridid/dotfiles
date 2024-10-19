f805206dddcd8fd2b4e4fd21fe27fbfc9934e175
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-10-19 19:53:41 -0400

    fix(dev): add missing gcc dependency to devshells

--- devshells/c-cpp.nix
+++ devshells/c-cpp.nix
@@ -4,6 +4,7 @@
     bear # For creating compilation databases for clangd
     clang-tools # Provides clangd lsp
     cmake # Needed for exercism C++ tests
+    gcc # Provides cc linker
     gnumake # Needed for exercism C and C++ tests
   ];
 }
--- devshells/rust.nix
+++ devshells/rust.nix
@@ -3,6 +3,7 @@
   packages = with pkgs; [
     cargo # Package manager
     clippy # Extra diagnostics, install globally for `cargo clippy` command
+    gcc # Provides linker cc
     rust-analyzer # Language server
     rustc # Compiler
     rustfmt # Formatter
