40d7a6f74e14b6749b42e4cf06567c2462edd451
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-11-08 21:10:10 -0500

    feat(dev): add pharo startup file

--- home-modules/development.nix
+++ home-modules/development.nix
@@ -62,4 +62,63 @@ in
         format = "yaml";
       }).configFile;
   };
+
+  xdg.configFile."pharo/startup.st".text =
+    let
+      inherit (config.stylix) fonts;
+    in
+    ''
+      StartupPreferencesLoader default executeAtomicItems: {
+        (StartupAction
+          name: 'Show any startup action errors'
+          code: [
+                  StartupPreferencesLoader default errors
+                    ifNotEmpty: [ :errors | errors inspect ] ]).
+
+        (StartupAction
+          name: 'Close welcome window'
+          code: [
+                  World submorphs
+                    select: [ :sm | sm isSystemWindow and: [ sm label endsWith: 'Welcome' ] ]
+                    thenDo: [ :window | window delete ] ]).
+
+        (StartupAction
+          name: 'Set theme'
+          code: [ PharoDarkTheme beCurrent ]).
+        
+        "TODO: Set text editing font"
+        "(StartupAction
+          name: 'Load font'
+          code: [
+                  FreeTypeFontProvider current updateEmbeddedFreeTypeFonts.
+                  FreeTypeFontProvider current updateFromSystem.
+
+                  StandardFonts setAllStandardFontsTo: (LogicalFont
+                    familyName: '${fonts.monospace.name}'
+                    pointSize: ${toString fonts.sizes.applications}) ])."
+
+        (StartupAction
+          name: 'Install TilingWindowManager'
+          code: [
+                  Metacello new
+                    githubUser: 'pdebruic' 
+                    project: 'TilingWindowManager' 
+                    commitish: 'master' 
+                    path: 'packages';
+                    baseline: 'TilingWindowManager';
+                    onWarningLog;
+                    load ]
+          runOnce: true).
+
+        (StartupAction
+          name: 'Install Exercism'
+          code: [
+                  Iceberg remoteTypeSelector: #httpsUrl.
+                  Metacello new
+                    baseline: 'Exercism';
+                    repository: 'github://exercism/pharo-smalltalk:main/releases/latest';
+                    load ]
+          runOnce: true).
+      }
+    '';
 }
