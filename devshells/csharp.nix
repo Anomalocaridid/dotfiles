e159552f6dcd936d3020142dd7409682faa56506
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-11-12 21:19:27 -0500

    fix(dev): disable .net telemetry and update dotnet_sdk version

--- devshells/csharp.nix
+++ devshells/csharp.nix
@@ -1,7 +1,15 @@
 { pkgs, ... }:
 {
   packages = with pkgs; [
-    dotnet-sdk_7 # Default version currently too low for Exercism
+    dotnet-sdk_8 # Default version is currently too low for Exercism
     omnisharp-roslyn # C# language server
+  ];
+
+  # Disable telemetry
+  env = [
+    {
+      name = "DOTNET_CLI_TELEMETRY_OPTOUT";
+      value = 1;
+    }
   ];
 }
--- devshells/fsharp.nix
+++ devshells/fsharp.nix
@@ -1,8 +1,16 @@
 { pkgs, ... }:
 {
   packages = with pkgs; [
-    dotnet-sdk_7 # Default version currently too low for Exercism
+    dotnet-sdk_8 # Default version currently too low for Exercism
     fantomas # F# code formatter
     fsautocomplete # F# language server
+  ];
+
+  # Disable telemetry
+  env = [
+    {
+      name = "DOTNET_CLI_TELEMETRY_OPTOUT";
+      value = 1;
+    }
   ];
 }
