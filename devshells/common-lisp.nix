b1718c3f140f8c15752728d926dc9a7f8ee29d1a
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-11-15 15:07:48 -0500

    feat(dev): add roswell for common lisp

--- devshells/common-lisp.nix
+++ devshells/common-lisp.nix
@@ -1,7 +1,15 @@
 { pkgs, ... }:
 {
   packages = with pkgs; [
+    roswell # Common Lisp build tool/test runner
     (sbcl.withPackages (ps: with ps; [ linedit ]))
-    gcc # Required to compile linedit
   ];
+
+  # Install necessary dependencies like quicklisp
+  # Rather noisy, so check if they need to be installed first
+  devshell.startup.roswell.text = ''
+    if [[ ! -d ~/.roswell ]]; then
+      ros help
+    fi
+  '';
 }
