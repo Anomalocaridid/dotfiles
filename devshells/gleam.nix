f3564310037f6dfa99feaed48394c2b0f3ef878b
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-12-13 16:53:04 -0500

    fix(dev): fix gleam devshell

--- devshells/gleam.nix
+++ devshells/gleam.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }:
 {
-  packages = with pkgs; [ gleam ];
+  packages = with pkgs; [
+    gleam
+    erlang_27 # Required by gleam, default version not recent enough for exercism tests
+  ];
 }
