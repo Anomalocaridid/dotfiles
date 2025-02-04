e0450ce9bfdb8c363f9030cf2bf139f7d12b4157
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-02-03 20:00:13 -0500

    refactor(yazi): move lua code to yazi.nix

--- home-modules/yazi.nix
+++ home-modules/yazi.nix
@@ -8,12 +8,54 @@
   programs.yazi = {
     enable = true;
     enableFishIntegration = true;
-    initLua = ./.config/yazi/init.lua;
+    initLua =
+      #lua
+      ''
+        require("full-border"):setup()
+        require("relative-motions"):setup { show_numbers = "relative_absolute", show_motion = "true" }
+        require("starship"):setup()
+
+        function Status:name()
+          local h = cx.active.current.hovered
+          if not h then
+            return ui.Span("")
+          end
+
+          -- Modified to show symlink in status bar
+          local linked = ""
+          if h.link_to ~= nil then
+            linked = " -> " .. tostring(h.link_to)
+          end
+          return ui.Span(" " .. h.name .. linked)
+        end
+
+        -- Add user/group of files in status bar
+        Status:children_add(function()
+          local h = cx.active.current.hovered
+          if h == nil or ya.target_family() ~= "unix" then
+            return ui.Line {}
+          end
+
+          return ui.Line {
+            ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+            ui.Span(":"),
+            ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+            ui.Span(" "),
+          }
+        end, 500, Status.RIGHT)
+      '';
     plugins =
-      (lib.attrsets.mapAttrs' (
-        x: _: lib.attrsets.nameValuePair (lib.strings.removeSuffix ".yazi" x) (./.config/yazi/plugins/${x})
-      ) (builtins.readDir ./.config/yazi/plugins))
-      // (with inputs; {
+      let
+        writePlugin =
+          text:
+          pkgs.writeTextFile rec {
+            inherit text;
+            name = "init.lua";
+            destination = "/${name}";
+          };
+      in
+      with inputs;
+      {
         # Previewers
         "glow" = glow-yazi;
         "miller" = miller-yazi;
@@ -28,7 +70,39 @@
         # UI enhancements
         "full-border" = yazi-plugins + /full-border.yazi;
         "smart-enter" = yazi-plugins + /smart-enter.yazi;
-      });
+        # Custom
+        "arrow" =
+          writePlugin
+            # lua
+            ''
+              --- @sync entry
+              return {
+                entry = function(_, job)
+                  local current = cx.active.current
+                  local new = (current.cursor + job.args[1]) % #current.files
+                  ya.manager_emit("arrow", { new - current.cursor })
+                end,
+              }
+            '';
+        "smart-paste" =
+          writePlugin
+            # lua
+            ''
+              --- @sync entry
+              return {
+                entry = function()
+                  local h = cx.active.current.hovered
+                  if h and h.cha.is_dir then
+                    ya.manager_emit("enter", {})
+                    ya.manager_emit("paste", {})
+                    ya.manager_emit("leave", {})
+                  else
+                    ya.manager_emit("paste", {})
+                  end
+                end,
+              }
+            '';
+      };
     settings = {
       plugin = {
         prepend_previewers =
