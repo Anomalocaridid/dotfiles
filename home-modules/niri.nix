c797bcb945d6ee481c55bef0f3f821b90ba32c58
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-02-11 17:00:43 -0500

    feat(niri): add custom shader animations

--- home-modules/niri.nix
+++ home-modules/niri.nix
@@ -111,6 +111,86 @@
         }
       ];
 
+      animations = {
+        window-open.easing = {
+          curve = "linear";
+          duration-ms = 350;
+        };
+        window-close.easing = {
+          curve = "linear";
+          duration-ms = 500;
+        };
+        shaders = {
+          window-open =
+            # glsl
+            ''
+              // Example: show the window as an expanding circle.
+              // Recommended setting: duration-ms 250
+              vec4 expanding_circle(vec3 coords_geo, vec3 size_geo) {
+                  vec3 coords_tex = niri_geo_to_tex * coords_geo;
+                  vec4 color = texture2D(niri_tex, coords_tex.st);
+
+                  vec2 coords = (coords_geo.xy - vec2(0.5, 0.5)) * size_geo.xy * 2.0;
+                  coords = coords / length(size_geo.xy);
+                  float p = niri_clamped_progress;
+                  if (p * p <= dot(coords, coords))
+                      color = vec4(0.0);
+
+                  return color;
+              }
+
+              // This is the function that you must define.
+              vec4 open_color(vec3 coords_geo, vec3 size_geo) {
+                  return expanding_circle(coords_geo, size_geo);
+              }
+            '';
+          window-close =
+            # glsl
+            ''
+              // Example: make the window 'fall down' with slight rotation.
+              vec4 fall_and_rotate(vec3 coords_geo, vec3 size_geo) {
+                  // For this shader, set animation curve to linear for best results.
+
+                  // Simulate an accelerated fall: square the (linear) progress.
+                  float progress = niri_clamped_progress * niri_clamped_progress;
+
+                  // Get our rotation pivot point coordinates at the bottom center of the window.
+                  vec2 coords = (coords_geo.xy - vec2(0.5, 1.0)) * size_geo.xy;
+
+                  // Move the window down to simulate a fall.
+                  coords.y -= progress * 1440.0;
+
+                  // Randomize rotation direction and maximum angle.
+                  float random = (niri_random_seed - 0.5) / 2.0;
+                  random = sign(random) - random;
+                  float max_angle = 0.5 * random;
+
+                  // Rotate the window around our pivot point.
+                  float angle = progress * max_angle;
+                  mat2 rotate = mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
+                  coords = rotate * coords;
+
+                  // Transform the coordinates back.
+                  coords_geo = vec3(coords / size_geo.xy + vec2(0.5, 1.0), 1.0);
+
+                  // Sample the window texture.
+                  vec3 coords_tex = niri_geo_to_tex * coords_geo;
+                  vec4 color = texture2D(niri_tex, coords_tex.st);
+
+                  // Multiply by alpha to fade out.
+                  // return color * (1.0 - niri_clamped_progress);
+                  return color;
+              }
+
+              // This is the function that you must define.
+              vec4 close_color(vec3 coords_geo, vec3 size_geo) {
+                  return fall_and_rotate(coords_geo, size_geo);
+              }
+            '';
+        };
+
+      };
+
       binds =
         let
           inherit (config.lib.niri) actions;
