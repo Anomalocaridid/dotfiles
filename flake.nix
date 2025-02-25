ed557fdea1a6d0f29c1a9c9046ad86c7a4af9513
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-02-24 21:10:58 -0500

    feat(nixos): use lix main branch

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
