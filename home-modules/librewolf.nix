5257a46857abebd83011f6b31431fe8361e5bc6f
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-02-25 16:39:17 -0500

    feat(librewolf): declaratively configure extensions where applicable

--- home-modules/librewolf.nix
+++ home-modules/librewolf.nix
@@ -58,23 +58,99 @@
           id = 1;
           # Un-break certain websites
           settings."webgl.disabled" = false;
-          extensions = with firefox-addons; [
+          extensions.packages = with firefox-addons; [
             canvasblocker # Recommended for enabled WebGL
             keepassxc-browser
           ];
         };
         default = {
-          extensions = with firefox-addons; [
-            awesome-rss
-            catppuccin-firefox
-            catppuccin-gh-file-explorer
-            darkreader
-            indie-wiki-buddy
-            keepassxc-browser
-            redirector
-            stylus
-            ublacklist
-          ];
+          # Needed to make config files
+          preConfig = ''
+
+          '';
+          extensions = {
+            # Override extension settings
+            # NOTE: Each extension also needs `force = true` to prevent file conflicts
+            force = true;
+            packages = with firefox-addons; [
+              catppuccin-firefox
+              catppuccin-gh-file-explorer
+              darkreader
+              indie-wiki-buddy
+              keepassxc-browser # NOTE: needs mutable settings
+              redirector
+              stylus
+              ublacklist
+            ];
+            settings =
+              let
+                fonts = config.stylix.fonts;
+                accent = config.catppuccin.accent;
+                palette =
+                  (lib.importJSON "${config.catppuccin.sources.palette}/palette.json")
+                  .${config.catppuccin.flavor}.colors;
+              in
+              {
+                "addon@darkreader.org" = {
+                  force = true;
+                  settings = {
+                    syncSettings = false;
+                    theme = {
+                      fontFamily = fonts.sansSerif.name;
+                      darkSchemeBackgroundColor = palette.base.hex;
+                      darkSchemeTextColor = palette.text.hex;
+                      selectionColor = palette.surface2.hex;
+                    };
+                    previewNewDesign = true;
+                  };
+                };
+
+                "redirector@einaregilsson.com" = {
+                  force = true;
+                  settings = {
+                    redirects = [
+                      {
+                        description = "[Farside] General Entry";
+                        exampleUrl = "https://m.youtube.com/watch?v=dQw4w9WgXcQ";
+                        # Normally automatically generated, but will not be properly generated if missing
+                        # Does not cause serious problems if missing, just mangles example in redirector list
+                        exampleResult = "https://farside.link/youtube.com/watch?v=dQw4w9WgXcQ";
+                        includePattern = "^(?:https?://)?(?:www\\.)?(?:\\w{2;}\\.)?(?:mobile\\.|m\\.)?((?:imdb|imgur|instagram|medium|odysee|quora|reddit|tiktok|translate\\.google|wikipedia|youtube)\\.(?:com|org|au|de|co|cn).*)$";
+                        redirectUrl = "https://farside.link/$1";
+                        patternType = "R"; # Regular expression
+                        # Required or redirector will not work
+                        appliesTo = [ "main_frame" ];
+                      }
+                    ];
+                  };
+                };
+
+                # Stylus
+                # TODO: figure out how to populate with Catppuccin userstyles
+                "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = {
+                  force = true;
+                  settings = {
+                    dbInChromeStorage = true; # required for Stylus
+                  };
+                };
+
+                "@ublacklist" = {
+                  force = true;
+                  settings = {
+                    subscriptions = {
+                      "0" = {
+                        name = "Main AI blocklist";
+                        url = "https://raw.githubusercontent.com/laylavish/uBlockOrigin-HUGE-AI-Blocklist/main/list_uBlacklist.txt";
+                      };
+                    };
+                    updateInterval = 60;
+                    linkColor = palette.blue.hex;
+                    blockColor = palette.red.hex;
+                    highlightColors = [ palette.${accent}.hex ];
+                  };
+                };
+              };
+          };
           search =
             let
               mkParams = lib.mapAttrsToList lib.nameValuePair;
