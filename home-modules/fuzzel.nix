3b9c54c2e7d79734ee43606d1a1f45603c98bd17
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-03-05 20:14:02 -0500

    feat(fuzzel): match niri borders

--- home-modules/fuzzel.nix
+++ home-modules/fuzzel.nix
@@ -3,6 +3,7 @@
   programs.fuzzel =
     let
       fonts = config.stylix.fonts;
+      niriSettings = config.programs.niri.settings;
     in
     {
       enable = true;
@@ -12,6 +13,14 @@
           dpi-aware = "no"; # appears really small otherwise
           icon-theme = config.gtk.iconTheme.name;
           terminal = "handlr launch x-scheme-handler/terminal --";
+          # Match default vertical-pad
+          horizontal-pad = 8;
+        };
+        border = {
+          width = niriSettings.layout.border.width;
+          # NOTE: Depends on window-rule order, chooses corner that it should match up with
+          radius = builtins.ceil (builtins.elemAt niriSettings.window-rules 0)
+            .geometry-corner-radius.top-right;
         };
       };
     };
