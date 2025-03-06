efbfb4a508cf661253ab0ce29aa782264eb8ce73
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-03-05 20:49:26 -0500

    feat(wlogout): match niri borders

--- home-modules/wlogout.nix
+++ home-modules/wlogout.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   ...
 }:
 {
@@ -7,22 +8,36 @@
 
   catppuccin.wlogout.extraStyle =
     let
-      convertIcon = (
-        entry: icon:
+      niriSettings = config.programs.niri.settings;
+      borderRadius = (builtins.elemAt niriSettings.window-rules 0).geometry-corner-radius;
+      borderWidth = "${toString niriSettings.layout.border.width}px";
+      override =
+        entry: icon: vertBorder: horiBorder:
         #css
         ''
           #${entry} {
+            /* Customize icon */
             background-image: url("${config.gtk.iconTheme.package}/share/icons/${config.gtk.iconTheme.name}/apps/scalable/${icon}.svg");
+            /* Customize outer border */
+            ${lib.optionalString (horiBorder != null)
+              #css
+              ''
+                border-${vertBorder}-${horiBorder}-radius: ${
+                  toString (builtins.ceil borderRadius."${vertBorder}-${horiBorder}")
+                };
+                border-${horiBorder}-width: ${borderWidth};''
+            }
+            border-${vertBorder}-width: ${borderWidth};
           }
-        ''
-      );
+        '';
     in
+    # css
     ''
-      ${convertIcon "lock" "system-lock-screen"}
-      ${convertIcon "hibernate" "system-suspend-hibernate"}
-      ${convertIcon "logout" "system-log-out"}
-      ${convertIcon "shutdown" "system-shutdown"}
-      ${convertIcon "suspend" "system-suspend"}
-      ${convertIcon "reboot" "system-reboot"}
+      ${override "lock" "system-lock-screen" "top" "left"}
+      ${override "hibernate" "system-suspend-hibernate" "bottom" "left"}
+      ${override "logout" "system-log-out" "top" null}
+      ${override "shutdown" "system-shutdown" "bottom" null}
+      ${override "suspend" "system-suspend" "top" "right"}
+      ${override "reboot" "system-reboot" "bottom" "right"}
     '';
 }
