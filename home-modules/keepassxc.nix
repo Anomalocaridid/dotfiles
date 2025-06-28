8085672c14a98943261280550def73450821b2e1
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-06-28 16:54:39 -0400

    refactor(keepassxc): use home manager options

--- home-modules/keepassxc.nix
+++ home-modules/keepassxc.nix
@@ -6,54 +6,52 @@
 }:
 let
   filename = "keepassxc.ini";
-  path = "keepassxc/${filename}";
-  mkKeepassConf =
-    data:
-    (inputs.nixago.lib.${pkgs.system}.make {
-      output = filename;
-      inherit data;
-    }).configFile;
 in
 {
-  home.packages = with pkgs; [
-    keepassxc
-  ];
 
-  home.file."${config.xdg.cacheHome}/${path}".source = mkKeepassConf {
-    General.LastActiveDatabase = "${config.home.homeDirectory}/Sync/Keepass Databases/Personal.kdbx";
+  programs.keepassxc = {
+    enable = true;
+    settings = {
+      General = {
+        ConfigVersion = 2;
+        MinimizeAfterUnlock = true;
+      };
+      Browser = {
+        Enabled = true;
+        UpdateBinaryPath = false;
+      };
+      GUI = {
+        # Use system theme
+        ApplicationTheme = "classic";
+        # Compact toolbar
+        CompactMode = true;
+        # Minimize to tray icon on close
+        MinimizeOnClose = true;
+        MinimizeToTray = true;
+        ShowTrayIcon = true;
+        TrayIconAppearance = "colorful";
+      };
+      PasswordGenerator = {
+        AdvancedMode = true;
+        Dashes = true;
+        Length = 128;
+        Logograms = true;
+        Math = true;
+      };
+      Security = {
+        # Use DuckDuckGo to download favicons for privacy
+        IconDownloadFallback = true;
+        LockDatabaseIdle = true;
+      };
+    };
   };
 
-  xdg.configFile.${path}.source = mkKeepassConf {
-    General = {
-      ConfigVersion = 2;
-      MinimizeAfterUnlock = true;
-    };
-    Browser = {
-      Enabled = true;
-      UpdateBinaryPath = false;
-    };
-    GUI = {
-      # Use system theme
-      ApplicationTheme = "classic";
-      # Compact toolbar
-      CompactMode = true;
-      # Minimize to tray icon on close
-      MinimizeOnClose = true;
-      MinimizeToTray = true;
-      ShowTrayIcon = true;
-      TrayIconAppearance = "colorful";
-    };
-    PasswordGenerator = {
-      AdvancedMode = true;
-      Dashes = true;
-      Length = 128;
-      Logograms = true;
-      Math = true;
-    };
-    Security = {
-      # Use DuckDuckGo to download favicons for privacy
-      IconDownloadFallback = true;
-      LockDatabaseIdle = true;
-    };
-  };
+  # Ensure password database is always selected
+  home.file."${config.xdg.cacheHome}/keepassxc/${filename}".source =
+    (inputs.nixago.lib.${pkgs.system}.make {
+      output = filename;
+      data = {
+        General.LastActiveDatabase = "${config.home.homeDirectory}/Sync/Keepass Databases/Personal.kdbx";
+      };
+    }).configFile;
 }
