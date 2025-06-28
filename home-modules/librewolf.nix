8aef0eb0039b44dd3e6ef2295f77a8cd82b9fc89
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-06-28 17:39:56 -0400

    feat(librewolf): add more redirects

--- home-modules/librewolf.nix
+++ home-modules/librewolf.nix
@@ -185,6 +185,33 @@
                   settings = {
                     redirects = [
                       {
+                        description = "FreeTube";
+                        exampleUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+                        # Normally automatically generated, but will not be properly generated if missing
+                        # Does not cause serious problems if missing, just mangles example in redirector list
+                        exampleResult = "freetube://https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+                        includePattern = "((https://)?(www\\.)?youtu(be\\.com|\\.be)/.*)";
+                        redirectUrl = "freetube://$1";
+                        patternType = "R"; # Regular expression
+                        # Required or redirector will not work
+                        appliesTo = [
+                          "main_frame"
+                        ];
+                      }
+                      {
+                        description = "Steam Client";
+                        exampleUrl = "https://store.steampowered.com/";
+                        # Normally automatically generated, but will not be properly generated if missing
+                        # Does not cause serious problems if missing, just mangles example in redirector list
+                        exampleResult = "steam://openurl/https://store.steampowered.com/";
+                        includePattern = "^(https://(.*\\.)?steam(powered|community).com/)$";
+                        redirectUrl = "steam://openurl/$1";
+                        patternType = "R"; # Regular expression
+                        appliesTo = [
+                          "main_frame"
+                        ];
+                      }
+                      {
                         description = "[Farside] General Entry";
                         exampleUrl = "https://m.youtube.com/watch?v=dQw4w9WgXcQ";
                         # Normally automatically generated, but will not be properly generated if missing
