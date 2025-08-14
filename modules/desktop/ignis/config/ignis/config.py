1e89fa66abb0ab22346ea2fa9d5f51bcae036585
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-08-13 20:27:22 -0400

    fix(ignis): update ignis config

--- modules/desktop/ignis/config/ignis/config.py
+++ modules/desktop/ignis/config/ignis/config.py
@@ -7,9 +7,8 @@ from datetime import datetime
 from time import gmtime, strftime
 
 import psutil  # Not included by default
-import unicodeit  # Not included by default
-import wm  # Custom module with constants from window manager config
-from ignis.app import IgnisApp
+import unicodeit  # Not included by default # pyright: ignore[reportMissingTypeStubs]
+import wm  # pyright: ignore[reportMissingImports] # Custom module with constants from window manager config
 
 from ignis.services.audio import AudioService
 from ignis.services.fetch import FetchService
@@ -20,9 +19,17 @@ from ignis.services.system_tray import SystemTrayItem, SystemTrayService
 from ignis import utils
 from ignis.variable import Variable
 from ignis import widgets
+from ignis.css_manager import CssManager, CssInfoPath
 
-app = IgnisApp.get_default()
-app.apply_css(f"{utils.get_current_dir()}/style.scss", "user")
+css_manager = CssManager.get_default()
+css_manager.apply_css(
+    CssInfoPath(
+        name="main",
+        path=f"{utils.get_current_dir()}/style.scss",
+        compiler_function=lambda path: utils.sass_compile(path=path),
+        priority="user",
+    )
+)
 
 audio = AudioService.get_default()
 fetch = FetchService.get_default()
@@ -592,53 +599,55 @@ def left_arrow() -> widgets.Arrow:
     return widgets.Icon(image="go-previous")
 
 
-def bar(monitor_id: int) -> widgets.Window:
-    monitor_name = utils.get_monitor(monitor_id).get_connector()
-    widgets.Window(
-        namespace=f"ignis_bar_{monitor_id}",
-        monitor=monitor_id,
-        exclusivity="exclusive",
-        anchor=["left", "top", "right"],
-        # NOTE: no margin_bottom because windows already have spacing
-        margin_top=wm.GAP_WIDTH,
-        margin_left=wm.GAP_WIDTH,
-        margin_right=wm.GAP_WIDTH,
-        child=widgets.CenterBox(
-            start_widget=widgets.Box(
-                css_classes=["left"],
-                child=[
-                    workspaces(monitor_name),
-                    right_arrow(),
-                    active_window(monitor_name),
-                    right_arrow(),
-                ],
+def bar(monitor_id: int) -> widgets.Window | None:
+    monitor_name = utils.get_monitor(monitor_id)
+    if monitor_name:
+        monitor_name = monitor_name.get_connector()
+        return widgets.Window(
+            namespace=f"ignis_bar_{monitor_id}",
+            monitor=monitor_id,
+            exclusivity="exclusive",
+            anchor=["left", "top", "right"],
+            # NOTE: no margin_bottom because windows already have spacing
+            margin_top=wm.GAP_WIDTH,
+            margin_left=wm.GAP_WIDTH,
+            margin_right=wm.GAP_WIDTH,
+            child=widgets.CenterBox(
+                start_widget=widgets.Box(
+                    css_classes=["left"],
+                    child=[
+                        workspaces(monitor_name),
+                        right_arrow(),
+                        active_window(monitor_name),
+                        right_arrow(),
+                    ],
+                ),
+                center_widget=widgets.Box(
+                    visible=mpris.bind("players", lambda value: len(value) != 0),
+                    child=[
+                        left_arrow(),
+                        media(monitor_id),
+                        right_arrow(),
+                    ],
+                ),
+                end_widget=widgets.Box(
+                    css_classes=["right"],
+                    child=[
+                        left_arrow(),
+                        volume(),
+                        left_arrow(),
+                        connections(),
+                        left_arrow(),
+                        statistics(),
+                        left_arrow(),
+                        tray(),
+                        left_arrow(),
+                        clock(monitor_id),
+                    ],
+                ),
             ),
-            center_widget=widgets.Box(
-                visible=mpris.bind("players", lambda value: len(value) != 0),
-                child=[
-                    left_arrow(),
-                    media(monitor_id),
-                    right_arrow(),
-                ],
-            ),
-            end_widget=widgets.Box(
-                css_classes=["right"],
-                child=[
-                    left_arrow(),
-                    volume(),
-                    left_arrow(),
-                    connections(),
-                    left_arrow(),
-                    statistics(),
-                    left_arrow(),
-                    tray(),
-                    left_arrow(),
-                    clock(monitor_id),
-                ],
-            ),
-        ),
-    )
+        )
 
 
 for i in range(utils.get_n_monitors()):
-    bar(i)
+    _ = bar(i)
--- modules/desktop/ignis/default.nix
+++ modules/desktop/ignis/default.nix
@@ -1,75 +1,78 @@
 { lib, inputs, ... }:
 {
-  unify.modules.ignis = {
-    # Required for ignis
-    nixos.services.gvfs.enable = true;
-    home =
-      { config, pkgs, ... }:
-      let
-        ignisPackage = inputs.ignis.packages.${pkgs.system}.ignis.override {
-          extraPackages = with pkgs.python313Packages; [
-            psutil
-            unicodeit
-          ];
+  unify.modules.ignis.home =
+    { config, pkgs, ... }:
+    {
+      imports = [ inputs.ignis.homeManagerModules.default ];
+      programs.ignis = {
+        enable = true;
+        configDir = ./config/ignis;
+
+        services = {
+          audio.enable = true;
+          network.enable = true;
         };
-      in
-      {
-        home.packages = [
-          ignisPackage
+
+        sass = {
+          enable = true;
+          useDartSass = true;
+        };
+
+        extraPackages = with pkgs.python313Packages; [
+          psutil
+          unicodeit
         ];
+      };
 
-        xdg.configFile =
-          let
-            niriSettings = config.programs.niri.settings;
-            borderWidth = toString niriSettings.layout.border.width;
-          in
-          {
-            "ignis/wm.py".text =
-              # python
-              ''
-                BORDER_WIDTH = ${borderWidth}
-                GAP_WIDTH = ${toString niriSettings.layout.gaps} - BORDER_WIDTH
-                # Assume scrolling both up and down have the same cooldown
-                SCROLL_COOLDOWN_MS = ${toString niriSettings.binds."Mod+WheelScrollUp".cooldown-ms}
-              '';
+      xdg.configFile =
+        let
+          niriSettings = config.programs.niri.settings;
+          borderWidth = toString niriSettings.layout.border.width;
+        in
+        {
+          "ignis/wm.py".text =
+            # python
+            ''
+              BORDER_WIDTH = ${borderWidth}
+              GAP_WIDTH = ${toString niriSettings.layout.gaps} - BORDER_WIDTH
+              # Assume scrolling both up and down have the same cooldown
+              SCROLL_COOLDOWN_MS = ${toString niriSettings.binds."Mod+WheelScrollUp".cooldown-ms}
+            '';
 
-            "ignis/wm.scss".text =
-              # scss
-              ''
-                @use "sass:string";
+          "ignis/wm.scss".text =
+            # scss
+            ''
+              @use "sass:string";
 
-                @mixin border {
-                	border: ${borderWidth}px solid string.unquote("@accent_color");
-                  // NOTE: Depends on window-rule order, chooses corner that it should match up with
-                	border-radius: ${toString (builtins.ceil (builtins.elemAt niriSettings.window-rules 0).geometry-corner-radius.top-right)}px;
-                }
-              '';
-            ignis = {
-              source = ./config/ignis;
-              recursive = true;
-            };
-          };
+              @mixin border {
+              	border: ${borderWidth}px solid string.unquote("@accent_color");
+                // NOTE: Depends on window-rule order, chooses corner that it should match up with
+              	border-radius: ${toString (builtins.ceil (builtins.elemAt niriSettings.window-rules 0).geometry-corner-radius.top-right)}px;
+              }
+            '';
+          # Needed to include above files
+          ignis.recursive = true;
+        };
 
-        # This lets the Ignis bar count as a tray for programs that rely on tray.target
-        systemd.user.services.ignis = {
-          Unit = {
-            Description = "Ignis bar";
-            PartOf = [
-              "tray.target"
-            ];
-            # NOTE: will error if wayland compositor is not started first
-            # NOTE: will also cause dependency issues if after graphical-session.target
-            After = "niri.service";
-            BindsTo = "graphical-session.target";
-          };
-          Service = {
-            ExecStart = "${lib.getExe ignisPackage} init";
-            Restart = "on-failure";
-          };
-          Install.RequiredBy = [
+      # This lets the Ignis bar count as a tray for programs that rely on tray.target
+      systemd.user.services.ignis = {
+        Unit = {
+          Description = "Ignis bar";
+          PartOf = [
             "tray.target"
           ];
+          # NOTE: will error if wayland compositor is not started first
+          # NOTE: will also cause dependency issues if after graphical-session.target
+          After = "niri.service";
+          BindsTo = "graphical-session.target";
         };
+        Service = {
+          ExecStart = "${lib.getExe config.programs.ignis.finalPackage} init";
+          Restart = "on-failure";
+        };
+        Install.RequiredBy = [
+          "tray.target"
+        ];
       };
-  };
+    };
 }
