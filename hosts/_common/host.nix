bbd6504f2416efa7a6ffffb2fc0ad5c8d6da643f
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-08-16 14:16:57 -0400

    refactor(nixos): deduplicate host configs

---
+++ hosts/_common/host.nix
@@ -0,0 +1,22 @@
+{
+  hostname,
+  disk,
+  memory,
+  facterReportPath,
+}:
+{ config, lib, ... }:
+rec {
+  unify.hosts.nixos.${hostname} = {
+    modules = lib.attrsets.attrValues config.unify.modules;
+    nixos = {
+      networking.hostName = hostname;
+      imports = [ flake.diskoConfigurations.${hostname} ];
+      facter.reportPath = facterReportPath;
+    };
+    users.anomalocaris.modules = config.unify.hosts.nixos.${hostname}.modules;
+  };
+
+  flake.diskoConfigurations.${hostname} = import ./disko.nix {
+    inherit disk memory;
+  };
+}
