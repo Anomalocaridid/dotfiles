2232238fa0527a8fe80940e385661e0ba56c8c86
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-08-28 21:11:04 -0400

    feat(development): add latex setup

---
+++ modules/development/latex.nix
@@ -0,0 +1,46 @@
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      devshells.latex.packages = with pkgs; [
+        tectonic # latex engine
+        texlab # latex lsp
+      ];
+    };
+
+  unify.modules.development.home =
+    { lib, pkgs, ... }:
+    {
+      # NOTE: only works for standalone LaTeX files, not Tectonic projects
+      programs.helix.languages.language-server.texlab.config.texlab = {
+        build = {
+          onSave = true;
+          executable = "tectonic";
+          args = [
+            "-X"
+            "compile"
+            "%f"
+            "--synctex"
+            "--keep-logs"
+            "--keep-intermediates"
+          ];
+          forwardSearchAfter = true;
+        };
+        forwardSearch = {
+          executable = "sioyek";
+          args = [
+            "--reuse-window"
+            "--execute-command"
+            "toggle_synctex"
+            "--inverse-search"
+            ''texlab inverse-search --input "%%1" --line %%2''
+            "--forward-search-file"
+            "%f"
+            "--forward-search-line"
+            "%l"
+            "%p"
+          ];
+        };
+      };
+    };
+}
