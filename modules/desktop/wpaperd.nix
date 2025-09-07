1304ba4683df852134f2ec824628d981994c38c6
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-09-07 11:25:46 -0400

    refactor(wpaperd): put wallpaper in global option

--- modules/desktop/wpaperd.nix
+++ modules/desktop/wpaperd.nix
@@ -1,7 +1,9 @@
-{ inputs, ... }:
+{ config, inputs, ... }:
 {
+  flake.meta.wallpaper = inputs.catppuccin-fractal-wallpapers + "/05.png";
+
   unify.modules.general.home.services.wpaperd = {
     enable = true;
-    settings.any.path = inputs.catppuccin-fractal-wallpapers + "/05.png";
+    settings.any.path = config.flake.meta.wallpaper;
   };
 }
--- modules/system/grub.nix
+++ modules/system/grub.nix
@@ -1,4 +1,7 @@
-{ inputs, ... }:
+{ config, ... }:
+let
+  inherit (config.flake.meta) wallpaper;
+in
 {
   unify.modules.general.nixos =
     {
@@ -9,25 +12,21 @@
     }:
     {
       boot.loader = {
-        grub =
-          let
-            wallpaper = inputs.catppuccin-fractal-wallpapers + "/05.png";
-          in
-          {
-            device = "nodev";
-            efiSupport = true;
-            # Change GRUB theme wallpaper
-            splashImage = lib.mkForce wallpaper;
-            theme = lib.mkForce (
-              pkgs.runCommand "catppuccin-grub-background" { } ''
-                cp --recursive --no-preserve=mode ${config.catppuccin.sources.grub}/share/grub/themes/catppuccin-${config.catppuccin.flavor}-grub-theme/ "$out"
+        grub = {
+          device = "nodev";
+          efiSupport = true;
+          # Change GRUB theme wallpaper
+          splashImage = lib.mkForce wallpaper;
+          theme = lib.mkForce (
+            pkgs.runCommand "catppuccin-grub-background" { } ''
+              cp --recursive --no-preserve=mode ${config.catppuccin.sources.grub}/share/grub/themes/catppuccin-${config.catppuccin.flavor}-grub-theme/ "$out"
 
-                # Replace background
-                rm "$out"/background.png
-                cp ${wallpaper} "$out"/background.png
-              ''
-            );
-          };
+              # Replace background
+              rm "$out"/background.png
+              cp ${wallpaper} "$out"/background.png
+            ''
+          );
+        };
         efi.canTouchEfiVariables = true;
       };
     };
