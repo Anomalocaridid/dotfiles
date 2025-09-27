01ef12dcb19757e247fab64c1d8110c46d75d680
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-09-27 17:24:27 -0400

    fix(sioyek): persist data

--- modules/media/sioyek.nix
+++ modules/media/sioyek.nix
@@ -1,27 +1,36 @@
+{ config, ... }:
+let
+  inherit (config.flake.meta) persistDir username;
+in
 {
-  unify.modules.general.home =
-    { lib, pkgs, ... }:
-    {
-      programs.sioyek = {
-        enable = true;
-        config = {
-          check_for_updates_on_startup = "1";
-          should_launch_new_window = "1";
-          new_command = "_print ${lib.getExe pkgs.yad} --print --type=RAW --filename=%{file_path}";
-        };
-        bindings = {
-          _print = "<C-p>";
-        };
-      };
+  unify.modules.general = {
+    # Persist sioyek document settings
+    nixos.environment.persistence.${persistDir}.users.${username}.directories = [
+      ".local/share/sioyek"
+    ];
 
-      xdg.mimeApps.defaultApplications =
-        let
-          desktopFile = "sioyek.desktop";
-        in
-        {
-          "application/epub+zip" = desktopFile;
-          "application/oxps" = desktopFile;
-          "application/pdf" = desktopFile;
+    home =
+      { lib, pkgs, ... }:
+      {
+        programs.sioyek = {
+          enable = true;
+          config = {
+            check_for_updates_on_startup = "1";
+            should_launch_new_window = "1";
+            new_command = "_print ${lib.getExe pkgs.yad} --print --type=RAW --filename=%{file_path}";
+          };
+          bindings._print = "<C-p>";
         };
-    };
+
+        xdg.mimeApps.defaultApplications =
+          let
+            desktopFile = "sioyek.desktop";
+          in
+          {
+            "application/epub+zip" = desktopFile;
+            "application/oxps" = desktopFile;
+            "application/pdf" = desktopFile;
+          };
+      };
+  };
 }
