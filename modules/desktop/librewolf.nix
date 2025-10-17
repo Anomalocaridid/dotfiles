80ecc4ef152f38d7b4b77c3a2730d8f6bf09dd22
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-10-17 13:03:39 -0400

    refactor(librewolf): reorganize settings

--- modules/desktop/librewolf.nix
+++ modules/desktop/librewolf.nix
@@ -55,8 +55,8 @@ in
             mkAutoconfigJs =
               prefs:
               lib.concatStrings (
-                lib.mapAttrsToList (name: value: ''
-                  lockPref("${name}", ${builtins.toJSON value}); 
+                lib.mapAttrsToListRecursive (path: value: ''
+                  lockPref("${lib.concatStringsSep "." path}", ${builtins.toJSON value}); 
                 '') prefs
               );
           in
@@ -70,95 +70,105 @@ in
               extraPrefs = mkAutoconfigJs {
                 # Only sync bookmarks and tabs
                 # Some of these are set by default, but they should be locked just in case
-                "services.sync.engine.bookmarks" = true;
-                "services.sync.engine.tabs" = true;
-                "services.sync.engine.addons" = false;
-                "services.sync.engine.addresses" = false;
-                "services.sync.engine.addresses.available" = false;
-                "services.sync.engine.creditcards" = false;
-                "services.sync.engine.creditcards.available" = false;
-                "services.sync.engine.history" = false;
-                "services.sync.engine.passwords" = false;
-                "services.sync.engine.prefs" = false;
+                services.sync.engine = {
+                  bookmarks = true;
+                  tabs = true;
+                  addons = false;
+                  addresses = false;
+                  "addresses.available" = false;
+                  creditcards = false;
+                  "creditcards.available" = false;
+                  history = false;
+                  passwords = false;
+                  prefs = false;
+                };
+
                 # Enable sync
-                "identity.fxaccounts.enabled" = true;
+                identity.fxaccounts.enabled = true;
+
                 # Automatically enable installed extensions
-                "extensions.autoDisableScopes" = 0;
-                # Set theme
-                # "extensions.activeThemeID" = "${catppuccin-firefox.addonId}";
-                # Use same search engine for private browsing
-                "browser.search.separatePrivateDefault" = false;
+                extensions.autoDisableScopes = 0;
+
                 # Clear history on shutdown
                 # This should be on by default but is not for some reason
-                "privacy.clearOnShutdown.history" = true;
-                "privacy.clearOnShutdown_v2.historyFormDataAndDownloads" = true;
-                # Customize toolbar
-                # NOTE: needs to be set as a string here so it is formatted properly in the config
-                "browser.uiCustomization.state" = builtins.toJSON {
-                  placements = {
-                    # Overflow dropdown
-                    widget-overflow-fixed-list = [ ];
-                    # Extensions dropdown
-                    unified-extensions-area = [
+                privacy = {
+                  clearOnShutdown.history = true;
+                  clearOnShutdown_v2.historyFormDataAndDownloads = true;
+                };
+
+                browser = {
+                  # Use same search engine for private browsing
+                  search.separatePrivateDefault = false;
+
+                  # Customize toolbar
+                  # NOTE: needs to be set as a string here so it is formatted properly in the config
+                  uiCustomization.state = builtins.toJSON {
+                    placements = {
+                      # Overflow dropdown
+                      widget-overflow-fixed-list = [ ];
+                      # Extensions dropdown
+                      unified-extensions-area = [
+                        "redirector_einaregilsson_com-browser-action"
+                        "_bbb880ce-43c9-47ae-b746-c3e0096c5b76_-browser-action"
+                        "_ublacklist-browser-action"
+                        "addon_darkreader_org-browser-action"
+                        "_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action"
+                        "_cb31ec5d-c49a-4e5a-b240-16c767444f62_-browser-action"
+                      ];
+                      # Toolbar
+                      nav-bar = [
+                        "back-button"
+                        "forward-button"
+                        "stop-reload-button"
+                        "sidebar-button"
+                        "customizableui-special-spring1"
+                        "vertical-spacer"
+                        "urlbar-container"
+                        "customizableui-special-spring2"
+                        "screenshot-button"
+                        "downloads-button"
+                        "fxa-toolbar-menu-button"
+                        "ublock0_raymondhill_net-browser-action"
+                        "keepassxc-browser_keepassxc_org-browser-action"
+                        "unified-extensions-button"
+                      ];
+                      toolbar-menubar = [
+                        "menubar-items"
+                      ];
+                      TabsToolbar = [
+                        "firefox-view-button"
+                        "tabbrowser-tabs"
+                        "new-tab-button"
+                        "alltabs-button"
+                      ];
+                      vertical-tabs = [ ];
+                      PersonalToolbar = [
+                        "personal-bookmarks"
+                      ];
+                    };
+                    seen = [
                       "redirector_einaregilsson_com-browser-action"
                       "_bbb880ce-43c9-47ae-b746-c3e0096c5b76_-browser-action"
                       "_ublacklist-browser-action"
                       "addon_darkreader_org-browser-action"
+                      "keepassxc-browser_keepassxc_org-browser-action"
+                      "ublock0_raymondhill_net-browser-action"
                       "_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action"
                       "_cb31ec5d-c49a-4e5a-b240-16c767444f62_-browser-action"
+                      "developer-button"
                     ];
-                    # Toolbar
-                    nav-bar = [
-                      "back-button"
-                      "forward-button"
-                      "stop-reload-button"
-                      "sidebar-button"
-                      "customizableui-special-spring1"
-                      "vertical-spacer"
-                      "urlbar-container"
-                      "customizableui-special-spring2"
-                      "screenshot-button"
-                      "downloads-button"
-                      "fxa-toolbar-menu-button"
-                      "ublock0_raymondhill_net-browser-action"
-                      "keepassxc-browser_keepassxc_org-browser-action"
-                      "unified-extensions-button"
+                    dirtyAreaCache = [
+                      "unified-extensions-area"
+                      "nav-bar"
+                      "vertical-tabs"
                     ];
-                    toolbar-menubar = [
-                      "menubar-items"
-                    ];
-                    TabsToolbar = [
-                      "firefox-view-button"
-                      "tabbrowser-tabs"
-                      "new-tab-button"
-                      "alltabs-button"
-                    ];
-                    vertical-tabs = [ ];
-                    PersonalToolbar = [
-                      "personal-bookmarks"
-                    ];
+                    currentVersion = 21;
+                    newElementCount = 4;
                   };
-                  seen = [
-                    "redirector_einaregilsson_com-browser-action"
-                    "_bbb880ce-43c9-47ae-b746-c3e0096c5b76_-browser-action"
-                    "_ublacklist-browser-action"
-                    "addon_darkreader_org-browser-action"
-                    "keepassxc-browser_keepassxc_org-browser-action"
-                    "ublock0_raymondhill_net-browser-action"
-                    "_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action"
-                    "_cb31ec5d-c49a-4e5a-b240-16c767444f62_-browser-action"
-                    "developer-button"
-                  ];
-                  dirtyAreaCache = [
-                    "unified-extensions-area"
-                    "nav-bar"
-                    "vertical-tabs"
-                  ];
-                  currentVersion = 21;
-                  newElementCount = 4;
                 };
               };
             };
+
             profiles = {
               # Separate profile to enable WebGL because it allows more fingerprinting
               gaming = {
