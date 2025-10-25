835dbdff9be3f1254165179920c5bc5f53d64c6a
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-10-24 21:14:53 -0400

    feat(git): add mergiraf

--- modules/terminal/git/default.nix
+++ modules/terminal/git/default.nix
@@ -2,15 +2,20 @@
 {
   flake.meta.gitHubUsername = "Anomalocaridid";
 
-  unify.modules.general.home.programs.git = {
-    enable = true;
-    userEmail = "29845794+Anomalocaridid@users.noreply.github.com";
-    userName = config.flake.meta.gitHubUsername;
-    extraConfig = {
-      init.defaultBranch = "main";
-      core.autocrlf = "input";
-      merge.conflictstyle = "diff3";
-      diff.colorMoved = "default";
+  unify.modules.general.home.programs = {
+    git = {
+      enable = true;
+      userEmail = "29845794+Anomalocaridid@users.noreply.github.com";
+      userName = config.flake.meta.gitHubUsername;
+      extraConfig = {
+        init.defaultBranch = "main";
+        core.autocrlf = "input";
+        merge.conflictstyle = "diff3";
+        diff.colorMoved = "default";
+      };
     };
+
+    # Syntax-aware git merge driver
+    mergiraf.enable = true;
   };
 }
