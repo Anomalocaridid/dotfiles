9d2a5b9ceb00099d49c6aaf7291a79c364b79b2f
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-11-07 19:59:37 -0500

    fix(ignis): fix missing mpris art and module disappearing when reloading

--- modules/desktop/ignis/default.nix
+++ modules/desktop/ignis/default.nix
@@ -13,71 +13,76 @@ in
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  unify.modules.general.home =
-    { config, pkgs, ... }:
-    {
-      imports = [ inputs.ignis.homeManagerModules.default ];
-      programs.ignis = {
-        enable = true;
-        configDir = ./config;
+  unify.modules.general = {
+    # Needed for mpris service to fetch art from url
+    nixos.services.gvfs.enable = true;
 
-        services = {
-          audio.enable = true;
-          network.enable = true;
-        };
-
-        sass = {
+    home =
+      { config, pkgs, ... }:
+      {
+        imports = [ inputs.ignis.homeManagerModules.default ];
+        programs.ignis = {
           enable = true;
-          useDartSass = true;
+          configDir = ./config;
+
+          services = {
+            audio.enable = true;
+            network.enable = true;
+          };
+
+          sass = {
+            enable = true;
+            useDartSass = true;
+          };
+
+          extraPackages = with pkgs.python313Packages; [ psutil ];
         };
 
-        extraPackages = with pkgs.python313Packages; [ psutil ];
-      };
+        xdg.configFile = {
+          "ignis/wm.py".text =
+            # python
+            ''
+              PERSIST_DIR = "${persistDir}"
+              GAP_WIDTH = ${toString wm.gapMinusBorder}
+              # Assume scrolling both up and down have the same cooldown
+              SCROLL_COOLDOWN_MS = ${toString wm.mouseCooldownMs}
+              WORKSPACES = ${toString wm.workspaces}
+            '';
 
-      xdg.configFile = {
-        "ignis/wm.py".text =
-          # python
-          ''
-            PERSIST_DIR = "${persistDir}"
-            GAP_WIDTH = ${toString wm.gapMinusBorder}
-            # Assume scrolling both up and down have the same cooldown
-            SCROLL_COOLDOWN_MS = ${toString wm.mouseCooldownMs}
-            WORKSPACES = ${toString wm.workspaces}
-          '';
+          "ignis/wm.scss".text =
+            # scss
+            ''
+              @use "sass:string";
 
-        "ignis/wm.scss".text =
-          # scss
-          ''
-            @use "sass:string";
+              @mixin border {
+              	border: ${toString wm.borderWidth}px solid string.unquote("@accent_color");
+              	border-radius: ${toString wm.windowCornerRadius}px;
+              }
+            '';
+          # Needed to include above files
+          ignis.recursive = true;
+        };
 
-            @mixin border {
-            	border: ${toString wm.borderWidth}px solid string.unquote("@accent_color");
-            	border-radius: ${toString wm.windowCornerRadius}px;
-            }
-          '';
-        # Needed to include above files
-        ignis.recursive = true;
-      };
-
-      # This lets the Ignis bar count as a tray for programs that rely on tray.target
-      systemd.user.services.ignis = {
-        Unit = {
-          Description = "Ignis bar";
-          PartOf = [
+        # This lets the Ignis bar count as a tray for programs that rely on tray.target
+        systemd.user.services.ignis = {
+          Unit = {
+            Description = "Ignis bar";
+            PartOf = [
+              "tray.target"
+            ];
+            # NOTE: will error if wayland compositor is not started first
+            # NOTE: will also cause dependency issues if after graphical-session.target
+            After = "niri.service";
+            BindsTo = "graphical-session.target";
+          };
+          Service = {
+            ExecStart = "${lib.getExe config.programs.ignis.finalPackage} init";
+            Restart = "on-failure";
+          };
+          Install.RequiredBy = [
             "tray.target"
           ];
-          # NOTE: will error if wayland compositor is not started first
-          # NOTE: will also cause dependency issues if after graphical-session.target
-          After = "niri.service";
-          BindsTo = "graphical-session.target";
         };
-        Service = {
-          ExecStart = "${lib.getExe config.programs.ignis.finalPackage} init";
-          Restart = "on-failure";
-        };
-        Install.RequiredBy = [
-          "tray.target"
-        ];
       };
-    };
+  };
 }
