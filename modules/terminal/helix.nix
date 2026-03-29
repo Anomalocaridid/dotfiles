1d30b546f663cc50f4063ae97365d10fa0a00cc5
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2026-03-29 14:49:42 -0400

    feat(helix): use terminal file picker

--- modules/terminal/helix.nix
+++ modules/terminal/helix.nix
@@ -12,32 +12,46 @@
 
     programs.helix = {
       enable = true;
-      settings.editor = {
-        bufferline = "multiple";
-        lsp.display-messages = true;
-        indent-guides.render = true;
-        cursorline = true;
-        cursorcolumn = true;
-        color-modes = true;
-        statusline = {
-          left = [
-            "mode"
-            "spinner"
-            "file-name"
-            "separator"
-            "file-modification-indicator"
-          ];
-          right = [
-            "diagnostics"
-            "separator"
-            "file-type"
-            "separator"
-            "selections"
-            "separator"
-            "position"
-            "file-encoding"
-          ];
-          separator = "";
+      settings = {
+        # Use terminal file picker
+        # NOTE: Assumes yazi is the file manager
+        keys.normal.C-y = [
+          ":sh rm -f /tmp/unique-ca1ea106"
+          '':insert-output yazi "%{buffer_name}" --chooser-file=/tmp/unique-ca1ea106''
+          '':sh printf "\x1b[?1049h\x1b[?2004h" > /dev/tty''
+          ":open %sh{cat /tmp/unique-ca1ea106}"
+          ":redraw"
+          ":set mouse false"
+          ":set mouse true"
+        ];
+
+        editor = {
+          bufferline = "multiple";
+          lsp.display-messages = true;
+          indent-guides.render = true;
+          cursorline = true;
+          cursorcolumn = true;
+          color-modes = true;
+          statusline = {
+            left = [
+              "mode"
+              "spinner"
+              "file-name"
+              "separator"
+              "file-modification-indicator"
+            ];
+            right = [
+              "diagnostics"
+              "separator"
+              "file-type"
+              "separator"
+              "selections"
+              "separator"
+              "position"
+              "file-encoding"
+            ];
+            separator = "";
+          };
         };
       };
     };
