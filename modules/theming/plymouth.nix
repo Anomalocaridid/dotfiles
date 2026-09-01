e30683e805790c712739e6976d793af590b078aa
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2026-09-01 14:46:11 -0400

    feat(plymouth): silent boot

--- modules/theming/plymouth.nix
+++ modules/theming/plymouth.nix
@@ -1,3 +1,14 @@
 {
-  unify.modules.htpc.nixos.boot.plymouth.enable = true;
+  unify.modules.htpc.nixos.boot = {
+    plymouth.enable = true;
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=auto"
+    ];
+  };
 }
