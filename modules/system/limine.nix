9a3312a8c5a5026e128612855086781af8648c58
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2026-09-02 19:25:56 -0400

    feat(limine): quiet on htpc

--- modules/system/limine.nix
+++ modules/system/limine.nix
@@ -3,42 +3,46 @@ let
   inherit (config.flake.meta) wallpaper;
 in
 {
-  unify.nixos =
-    {
-      config,
-      lib,
-      pkgs,
-      ...
-    }:
-    {
-      # Interferes with setting background transparency
-      catppuccin.limine.enable = false;
+  unify = {
+    modules.htpc.nixos.boot.loader.limine.extraConfig = "quiet: yes";
 
-      boot.loader = {
-        limine = {
-          enable = true;
-          style.wallpapers = [ wallpaper ];
+    nixos =
+      {
+        config,
+        lib,
+        pkgs,
+        ...
+      }:
+      {
+        # Interferes with setting background transparency
+        catppuccin.limine.enable = false;
 
-          # Set background transparency
-          extraConfig = builtins.readFile (
-            pkgs.runCommand "catppuccin-limine"
-              {
-                themeFile =
-                  with config.catppuccin;
-                  "${sources.limine}/${flavor}/catppuccin-${flavor}-${accent}.conf";
+        boot.loader = {
+          limine = {
+            enable = true;
+            style.wallpapers = [ wallpaper ];
 
-                # 10% transparency (i.e. 90% opacity)
-                transparency = "19";
-              }
-              ''
-                cp $themeFile $out
-                substituteInPlace $out \
-                  --replace-fail "term_background: " "term_background: $transparency"
-              ''
-          );
+            # Set background transparency
+            extraConfig = builtins.readFile (
+              pkgs.runCommand "catppuccin-limine"
+                {
+                  themeFile =
+                    with config.catppuccin;
+                    "${sources.limine}/${flavor}/catppuccin-${flavor}-${accent}.conf";
+
+                  # 10% transparency (i.e. 90% opacity)
+                  transparency = "19";
+                }
+                ''
+                  cp $themeFile $out
+                  substituteInPlace $out \
+                    --replace-fail "term_background: " "term_background: $transparency"
+                ''
+            );
+          };
+
+          efi.canTouchEfiVariables = true;
         };
-
-        efi.canTouchEfiVariables = true;
       };
-    };
+  };
 }
