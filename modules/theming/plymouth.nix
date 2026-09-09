5a7458dcbe42a1fda52d94c938f956e9b596602f
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2026-09-09 13:53:01 -0400

    feat(nixos): add plymouth

---
+++ modules/theming/plymouth.nix
@@ -0,0 +1,14 @@
+{
+  unify.modules.appliance.nixos.boot = {
+    plymouth.enable = true;
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=auto"
+    ];
+  };
+}
